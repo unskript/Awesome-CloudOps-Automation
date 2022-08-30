751c44230323931ddd22e7749a2a715dbd67ef1a
Harshal Bangre <harsh@Harshal.localdomain>
2022-08-31 02:32:27 +0530

    EN-3093 - Rollback k8s deployment and update Jira

---
+++ Jira/legos/jira_issue_change_status/README.md
@@ -0,0 +1,21 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>Change JIRA Issue Status </h1>
+
+## Description
+This Lego Change JIRA Issue Status and update status.
+
+
+## Lego Details
+
+    jira_issue_change_status(handle: object, issue_id: str, status: str, transition: str)
+
+        handle: Object of type unSkript AWS Connector.
+        issue_id: Issue ID needed to Update.
+        status: New status to update.
+
+## Lego Input
+This Lego takes take two input issue_id and status.
+
+## See it in Action
+
+You can see this Lego in action following this link [unSkript Live](https://unskript.com)
---
+++ Jira/legos/jira_issue_change_status/jira_issue_change_status.py
@@ -0,0 +1,59 @@
+##
+# Copyright (c) 2021 unSkript, Inc
+# All rights reserved.
+##
+
+from jira.client import JIRA
+from pydantic import BaseModel, Field
+from typing import Optional
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+from beartype import beartype
+def legoPrinter(func):
+    def Printer(*args, **kwargs):
+        output = func(*args, **kwargs)
+        print('\n')
+        return output
+    return Printer
+
+
+@legoPrinter
+@beartype
+def jira_issue_change_status(hdl: JIRA, issue_id: str, status: str, transition: str = ""):
+    """jira_get_issue_status gets the status of a given Jira issue.
+        :type issue_id: str
+        :param issue_id: ID of the issue whose status we want to fetch (eg ENG-14)
+        :rtype: String with issue status fetched from JIRA API
+    """
+
+    # Input param validation.
+    issue = hdl.issue(issue_id)
+
+    if transition:
+        transitions = hdl.transitions(issue)
+        t = [t for t in transitions if t.get('name') == status]
+
+        if len(t) > 1:
+            print("Multiple transitions possible for JIRA issue. Please select transition number to use", [
+                t.get('id') for t in transitions if t.get('name') == status])
+            return
+        else:
+            transition = t[0].get('id')
+
+    hdl.transition_issue(issue, transition)
+    return
+
+
+def unskript_default_printer(output):
+    if isinstance(output, (list, tuple)):
+        for item in output:
+            print(f'item: {item}')
+    elif isinstance(output, dict):
+        for item in output.items():
+            print(f'item: {item}')
+    else:
+        print(f'Output for {task.name}')
+        print(output)
