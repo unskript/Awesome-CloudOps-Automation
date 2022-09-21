78689f3f4ef5a88ddea9b5e556eedb0ea51c16d8
shloka-bhalgat-unskript <110628398+shloka-bhalgat-unskript@users.noreply.github.com>
2022-09-21 18:15:17 +0000

    Feature/en 3170 shloka (#19)
    Co-authored-by: Shloka Bhalgat <shlokabhalgat@192.168.1.3>
    Co-authored-by: Shloka Bhalgat <shlokabhalgat@192.168.1.2>

---
+++ AWS/legos/aws_delete_access_key/README.md
@@ -0,0 +1,25 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>AWS Delete Access Key</h1>
+
+## Description
+This Lego deleted the old(Inactive) Access Key for the user.
+
+
+## Lego Details
+
+    aws_delete_access_key(handle,aws_username: str)
+
+        handle: Object of type unSkript AWS Connector.
+        aws_username: User name of the AWS user.
+        aws_access_key_id: Old Access Key ID of the User.
+
+
+## Lego Input
+This Lego take three inputs handle, aws_username, and aws_access_key_id.
+
+## Lego Output
+<img src="./1.png">
+
+## See it in Action
+
+You can see this Lego in action following this link [unSkript Live](https://unskript.com)
---
+++ AWS/legos/aws_update_access_keys/aws_update_access_keys.py
@@ -0,0 +1,58 @@
+##
+# Copyright (c) 2021 unSkript, Inc
+# All rights reserved.
+##
+from pydantic import BaseModel, Field, SecretStr
+from typing import Dict,List
+import pprint
+
+
+class InputSchema(BaseModel):
+    aws_username: str = Field(
+        title="Username",
+        description="Username of the IAM User"
+    )
+    aws_access_key_id: str = Field(
+        title="Access Key ID",
+        description="Old Access Key ID of the User"
+    )
+    status: str = Field(
+        title="Status",
+        description="Status to set for the Access Key"
+    )
+
+
+def aws_update_access_key_printer(output):
+    if output is None:
+        return
+    pprint.pprint("Access Key status successfully changed")
+    pprint.pprint(output)
+
+
+def aws_update_access_key(
+    handle,
+    aws_username: str,
+    aws_access_key_id: str,
+    status: str
+) -> Dict:
+    """aws_update_access_key updates the status of an access key to Inactive/Active
+                    :type handle: object
+                    :param handle: Object returned from Task Validate
+
+                    :type aws_username: str
+                    :param aws_username: Username of the IAM user to be looked up
+
+                    :type aws_access_key_id: str
+                    :param aws_access_key_id: Old Access Key ID of the user of which the status needs to be updated
+
+                    :rtype: Result Dictionary of result
+    """
+    iamClient = handle.client('iam')
+    result = iamClient.update_access_key(UserName=aws_username, AccessKeyId=aws_access_key_id, Status=status)
+    retVal = {}
+    temp_list = []
+    for key, value in result.items():
+        if key not in temp_list:
+            temp_list.append(key)
+            retVal[key] = value
+    return retVal
