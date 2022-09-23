3578c177321e5695fe699f1f7c9f4731a72031e1
Harshal Bangre <harsh@Harshal.localdomain>
2022-09-23 17:14:47 +0530

    EN-1745 - Run EC2 from AMI

---
+++ AWS/legos/aws_launch_instance_from_ami/aws_launch_instance_from_ami.py
@@ -0,0 +1,43 @@
+##
+##  Copyright (c) 2021 unSkript, Inc
+##  All rights reserved.
+##
+from pydantic import BaseModel, Field
+from typing import List
+import pprint
+
+
+class InputSchema(BaseModel):
+    ami_id: str = Field(
+        title='AMI Id',
+        description='AMI Id.')
+    region: str = Field(
+        title='Region',
+        description='AWS Region.')
+
+
+def aws_launch_instance_from_ami_printer(output):
+    if output is None:
+        return
+    pprint.pprint(output)
+
+
+def aws_launch_instance_from_ami(handle, ami_id: str, region: str) -> List:
+    """aws_launch_instance_from_ami Launch instances from a particular image.
+
+        :type nbParamsObj: object
+        :param nbParamsObj: Object containing global params for the notebook.
+
+        :type credentialsDict: dict
+        :param credentialsDict: Dictionary of credentials info.
+
+        :type inputParamsJson: string
+        :param inputParamsJson: Json string of the input params.
+
+        :rtype: Dict with launched instances info.
+    """
+    ec2Client = handle.client('ec2', region_name=region)
+
+    res = ec2Client.run_instances(ImageId=ami_id, MinCount=1, MaxCount=1)
+
+    return res['Instances']
