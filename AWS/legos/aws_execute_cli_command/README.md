f4f32ad2da45582ee3a6cf9136a0cbf91c6532c5
Harshal Bangre <harsh@Harshal.localdomain>
2022-09-28 18:30:33 +0530

    EN-3082 - Lowering AWS CloudTrail Costs by Removing Redundant Trails

---
+++ AWS/legos/aws_execute_cli_command/README.md
@@ -0,0 +1,25 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>Run Command via AWS CLI </h1>
+
+## Description
+This Lego find the Redundant Trails in AWS and returns list of trail details.
+
+## Lego Details
+
+    aws_execute_cli_command(handle, aws_command: str)
+
+        handle: Object of type unSkript AWS Connector
+        aws_command: AWS command.
+
+## Lego Input
+This Lego take two inputs handle, aws_command.
+
+## Lego Output
+Here is a sample output.
+
+<img src="./1.png">
+
+
+## See it in Action
+
+You can see this Lego in action following this link [unSkript Live](https://unskript.com)
---
+++ AWS/legos/aws_execute_cli_command/aws_execute_cli_command.py
@@ -0,0 +1,32 @@
+#
+# Copyright (c) 2021 unSkript.com
+# All rights reserved.
+#
+
+from pydantic import BaseModel, Field
+import pprint
+
+
+class InputSchema(BaseModel):
+    aws_command: str = Field(
+        title='AWS Command',
+        description='AWS Command '
+                    'eg "aws ec2 describe-instances"'
+    )
+
+
+def aws_execute_cli_command_printer(output):
+    if output is None:
+        return
+    pprint.pprint(output)
+
+
+def aws_execute_cli_command(handle, aws_command: str) -> str:
+
+    result = handle.aws_cli_command(aws_command)
+    if result is None or result.returncode != 0:
+        print(
+            f"Error while executing command ({aws_command}): {result}")
+        return str()
+
+    return result.stdout
