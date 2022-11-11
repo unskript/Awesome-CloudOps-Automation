6e0ec5f2e10118da3685113514998e106edea88a
shloka-bhalgat-unskript <shloka@unskript.com>
2022-11-11 18:26:15 +0530

    GCP: List all GCP Buckets Lego

---
+++ GCP/legos/gcp_list_buckets/gcp_list_buckets.py
@@ -0,0 +1,33 @@
+##
+##  Copyright (c) 2021 unSkript, Inc
+##  All rights reserved.
+##
+from pydantic import BaseModel, Field
+import pprint
+from typing import List,Any, Dict
+from google.cloud import storage
+
+class InputSchema(BaseModel):
+    pass
+
+def gcp_list_buckets_printer(output):
+    if len(output)==0:
+        print("There are no buckets available")
+        return
+    pprint.pprint(output)
+    
+
+def gcp_list_buckets(handle) -> List:
+    """gcp_list_buckets lists all GCP Buckets
+
+        :rtype: List of all GCP buckets
+    """
+    try:
+        result=[]
+        storage_client = storage.Client(credentials=handle)
+        buckets = storage_client.list_buckets()
+        for bucket in buckets:
+            result.append(bucket.name)
+    except Exception as e:
+        result["error"]= e
+    return result
