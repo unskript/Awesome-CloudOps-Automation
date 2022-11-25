2c5c9d1624fc38bd115322596bf982cdd294a7fb
Harshal Bangre <harsh@Harshal.localdomain>
2022-11-25 19:17:13 +0530

    EN-3441 - EBS Delete RunBook does not give option to snapshot

--- AWS/legos/aws_delete_volume/aws_delete_volume_by_id.py
+++ AWS/legos/aws_delete_volume/aws_delete_volume_by_id.py
@@ -10,29 +10,31 @@ import pprint
 class InputSchema(BaseModel):
     volume_id: str = Field(
         title='Volume ID',
-        description='Volume ID.')
+        description='Volume ID is needed to delete a particular volume e.g. vol-01eb21cfce30a956c.')
     region: str = Field(
         title='Region',
         description='AWS Region.')
+
 
 def aws_delete_volumes_printer(output):
     if output is None:
         return
     pprint.pprint({"Output": output})
 
-def aws_delete_volumes(handle, volume_id: str, region: str) -> str:
-    """aws_filter_ebs_unattached_volumes Returns an array of ebs volumes.
+
+def aws_delete_volumes(handle, volume_id: str, region: str) -> List:
+    """aws_delete_volumes Returns array of delete volume status.
 
         :type handle: object
         :param handle: Object returned by the task.validate(...) method.
 
         :type region: string
-        :param region: Used to filter the volume for specific region.
+        :param region: used to filter the volume for a given region.
 
         :type volume_id: string
-        :param volume_id: Volume ID needed to delete particular volume.
+        :param volume_id: Volume ID to delete a given volume.
 
-        :rtype: Result of the API in the List form.
+        :rtype: Array of delete volume status.
     """
     result = []
 
@@ -46,3 +48,4 @@ def aws_delete_volumes(handle, volume_id: str, region: str) -> str:
         result.append(e)
 
     return result
+
