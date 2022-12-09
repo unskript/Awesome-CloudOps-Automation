5bd975839ca3b861e9151416916b44c18b01a578
shloka-bhalgat-unskript <shloka@unskript.com>
2022-12-09 16:17:28 +0530

    EN-3671 List all IAM Users Lego

---
+++ AWS/legos/aws_list_all_iam_users/aws_list_all_iam_users.py
@@ -0,0 +1,33 @@
+##
+# Copyright (c) 2021 unSkript, Inc
+# All rights reserved.
+##
+from pydantic import BaseModel, Field, SecretStr
+from typing import Dict, List
+import pprint
+
+class InputSchema(BaseModel):
+    pass
+
+def aws_list_all_iam_users_printer(output):
+    if output is None:
+        return
+    pprint.pprint(output)
+
+def aws_list_all_iam_users(handle) -> List:
+    """aws_list_all_iam_users lists all the IAM users
+
+        :type handle: object
+        :param handle: Object returned from Task Validate
+        
+        :rtype: Result List of result
+    """
+    client = handle.client('iam') 
+    users_list=[]
+    response = client.list_users()
+    try:
+        for x in response['Users']:
+            users_list.append(x['UserName'])
+    except Exception as e:
+        users_list.append(e)
+    return users_list
