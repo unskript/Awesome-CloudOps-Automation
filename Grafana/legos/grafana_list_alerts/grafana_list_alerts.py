f6505d28d8996c95f702965ced6f13c1eb1f05c2
amit-chandak-unskript <79171703+amit-chandak-unskript@users.noreply.github.com>
2022-12-22 14:11:56 -0800

    EN-3704-ts-grafana-not-all-alarms-are-pulled-in (#160)

--- Grafana/legos/grafana_list_alerts/grafana_list_alerts.py
+++ Grafana/legos/grafana_list_alerts/grafana_list_alerts.py
@@ -3,6 +3,7 @@
 # All rights reserved.
 ##
 import json
+
 import pprint
 from typing import Optional, List
 
@@ -45,16 +46,17 @@ def grafana_list_alerts(handle: Grafana, dashboard_id:int = None,
        :rtype: List of alerts.
     """
     url = handle.host + "/api/ruler/grafana/api/v1/rules"
+    params = None
     if dashboard_id or panel_id:
         param = {}
         if dashboard_id:
             param["DashboardUID"] = dashboard_id
         if panel_id:
             param["PanelID"] = panel_id
-        response = handle.session.get(url,
-                                      params=param)
-    else:
-        response = handle.session.get(url)
+        params = param
+
+    response = handle.session.get(url,
+                                 params=params)
 
     result = []
     folder_names = json.loads(response.content).keys()
@@ -64,4 +66,53 @@ def grafana_list_alerts(handle: Grafana, dashboard_id:int = None,
             res['id'] = alarm['rules'][0]['grafana_alert']['id']
             res['name'] = alarm['name']
             result.append(res)
+    # Get Loki alerts as well.
+    # First get the datasources which has type Loki.
+    url = handle.host + "/api/datasources"
+    try:
+        response = handle.session.get(url)
+        response.raise_for_status()
+    except Exception as e:
+        # This could happen because in non-cloud grafana, there is no rbac, so this api doesnt work with viewer role.
+        print("Unable to get datasources")
+        return result
+
+    try:
+        datasourcesList = json.loads(response.content)
+    except Exception as e:
+        print(f'Unable to parse datasources response, error {str(e)}')
+        return result
+
+    lokiDatasourcesList = []
+    for datasource in datasourcesList:
+        if datasource["type"] == "loki":
+            lokiDatasourcesList.append(datasource["uid"])
+
+    for lokiDatasourceUID in lokiDatasourcesList:
+        url = handle.host + "/api/ruler/" + lokiDatasourceUID + "/api/v1/rules"
+        try:
+            response = handle.session.get(url, params=params)
+            response.raise_for_status()
+        except Exception as e:
+            print(f'Failed to get rules for datasource uid {lokiDatasourceUID}, error {str(e)}')
+            continue
+
+        try:
+            responseDict = json.loads(response.content)
+        except Exception as e:
+            print(f'Unable to parse rules response, uid {lokiDatasourceUID}, error {str(e)}')
+            continue
+
+        folder_names = responseDict.keys()
+        for folder_name in list(folder_names):
+            for alarm in responseDict[folder_name]:
+                for rule in alarm['rules']:
+                    res = {}
+                    # Loki have 'alert' in the key, where as recorded loki has 'record' as the key.
+                    if 'alert' in rule:
+                        res['name'] = rule['alert']
+                    else:
+                        res['name'] = rule['record']
+                    result.append(res)
+
     return result
