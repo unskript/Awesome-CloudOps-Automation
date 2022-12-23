2906c0ff72bbe0cdb16e2057ef66b618ebe02da5
shloka-bhalgat-unskript <shloka@unskript.com>
2022-12-23 13:49:18 +0530

    EN-3738 Check Lego to Filter Unused Key Pairs

--- AWS/legos/aws_filter_unused_keypairs/aws_filter_unused_keypairs.py
+++ AWS/legos/aws_filter_unused_keypairs/aws_filter_unused_keypairs.py
@@ -4,6 +4,7 @@
 ##
 from pydantic import BaseModel, Field
 from typing import List
+from unskript.legos.aws.aws_list_all_regions.aws_list_all_regions import aws_list_all_regions
 from unskript.connectors.aws import aws_get_paginator
 import pprint
 
@@ -19,34 +20,46 @@ def aws_filter_unused_keypairs_printer(output):
     pprint.pprint({"Instances": output})
 
 
-def aws_filter_unused_keypairs(handle, region: str) -> List:
+def aws_filter_unused_keypairs(handle, region: str = None) -> List:
     """aws_filter_unused_keypairs Returns an array of KeyPair.
 
-        :type handle: object
-        :param handle: Object returned from task.validate(...).
-        
         :type region: object
         :param region: Object containing global params for the notebook.
 
-        :rtype: Array of instances matching tags.
+        :rtype: List of unused key pairs.
     """
+    all_keys_dict = {}
+    used_keys_dict = {}
     key_pairs_all = []
     used_key_pairs = []
-    ec2Client = handle.client('ec2', region_name=region)
-
-    # List the key pairs in the selected region
-    key_pairs_all = list(map(lambda i: i['KeyName'], ec2Client.describe_key_pairs()['KeyPairs']))
-
-    # Get group of EC2 instances.
-    res = aws_get_paginator(ec2Client, "describe_instances", "Reservations")
-    for reservation in res:
-        for keypair in reservation['Instances']:
-            if 'KeyName'in keypair and keypair['KeyName'] not in used_key_pairs:
-                used_key_pairs.append(keypair['KeyName'])
-
-    # Compaire key_pairs_all and used_key_pairs list
     result = []
-    for key in key_pairs_all:
-        if key not in used_key_pairs:
-            result.append(key)
+    all_regions = [region]
+    if region is None or not region:
+        all_regions = aws_list_all_regions(handle=handle)
+    for r in all_regions:
+        try:
+            ec2Client = handle.client('ec2', region_name=r)
+            key_pairs_all = list(map(lambda i: i['KeyName'], ec2Client.describe_key_pairs()['KeyPairs']))
+            res = aws_get_paginator(ec2Client, "describe_instances", "Reservations")
+            for reservation in res:
+                for keypair in reservation['Instances']:
+                    if 'KeyName'in keypair and keypair['KeyName'] not in used_key_pairs:
+                        used_key_pairs.append(keypair['KeyName'])
+            used_keys_dict["region"]=r
+            used_keys_dict["key_name"]=used_key_pairs
+            all_keys_dict["region"]=r
+            all_keys_dict["key_name"]=key_pairs_all
+            final_dict = {}
+            final_list=[]
+            for k,v in all_keys_dict.items():
+                if v!=[]:
+                    if k=="key_name":
+                        for each in v:
+                            if each not in used_keys_dict["key_name"]:
+                                final_list.append(each)
+                final_dict["region"]=r
+                final_dict["unused_keys"]=final_list
+            result.append(final_dict)
+        except Exception as e:
+            pass
     return result
