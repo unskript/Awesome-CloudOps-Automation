439174d0d6591be910256131d921b8f7bd1402d8
Harshal Bangre <harsh@Harshal.localdomain>
2022-12-26 19:38:30 +0530

    Updated check lego

--- Kubernetes/legos/k8s_get_all_evicted_pods_from_namespace/k8s_get_all_evicted_pods_from_namespace.py
+++ Kubernetes/legos/k8s_get_all_evicted_pods_from_namespace/k8s_get_all_evicted_pods_from_namespace.py
@@ -6,8 +6,7 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
 import pprint
-import pandas as pd
-import io
+import json
 
 class InputSchema(BaseModel):
     namespace: Optional[str] = Field(
@@ -37,21 +36,32 @@ def k8s_get_all_evicted_pods_from_namespace(handle, namespace: str = "") -> List
         print(f"K8S Connector is invalid: {handle}")
         return str()
 
-    kubectl_command = "kubectl get pods --all-namespaces | grep Evicted"
+    kubectl_command = "kubectl get pods --all-namespaces -o json | grep Evicted"
     if namespace:
-        kubectl_command = "kubectl get pods -n " + namespace + " | grep Evicted"
+        kubectl_command = "kubectl get pods -n " + namespace + " -o json | grep Evicted"
 
-    result = handle.run_native_cmd(kubectl_command)
-    if result is None or hasattr(result, "stderr") is False or result.stderr is None:
+    response = handle.run_native_cmd(kubectl_command)
+    if response is None or hasattr(response, "stderr") is False or response.stderr is None:
         print(
-            f"Error while executing command ({kubectl_command}): {result.stderr}")
+            f"Error while executing command ({kubectl_command}): {response.stderr}")
         return str()
 
-    all_pods = []
+    result = []
     try:
-        df = pd.read_fwf(io.StringIO(result.stdout))
-        for index, row in df.iterrows():
-            all_pods.append(row['NAME'])
-        return all_pods
+        pod_details = json.loads(response.stdout)
+        for k, v in pod_details.items():
+            if "items" in k:
+                for i in v:
+                    pod_dict = {}
+                    pod_dict["pod_name"] = i["metadata"]["name"]
+                    pod_dict["namespace"] = i["metadata"]["namespace"]
+                    result.append(pod_dict)
     except Exception as e:
-        return all_pods
+        pass
+    
+    execution_flag = False
+    if len(result) > 0:
+        execution_flag = True
+    output = (execution_flag, result)
+    return output
+    
