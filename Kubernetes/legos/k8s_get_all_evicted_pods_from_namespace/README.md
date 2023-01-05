08cc511c8204f80fffe411c333c2bd3624073500
harshal-netserv <63058785+harshal-netserv@users.noreply.github.com>
2023-01-05 11:21:03 +0530

    Kubernetes Delete Evicted pods runbook (#170)
    Co-authored-by: Harshal Bangre <harsh@Harshal.localdomain>

---
+++ Kubernetes/legos/k8s_get_all_evicted_pods_from_namespace/README.md
@@ -0,0 +1,27 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>Get All Evicted PODS From Namespace </h1>
+
+## Description
+This Lego get all evicted PODS from given namespace. If namespace not given it will get all the pods from all namespaces.
+
+
+## Lego Details
+
+    k8s_get_all_evicted_pods_from_namespace(handle, namespace: str = "")
+
+        handle: Object of type unSkript K8S Connector
+        namespace: k8s namespace.
+
+## Lego Input
+
+This Lego take two inputs handle, and namespace.
+
+
+## Lego Output
+Here is a sample output.
+<img src="./1.png">
+
+
+## See it in Action
+
+You can see this Lego in action following this link [unSkript Live](https://us.app.unskript.io)
---
+++ Kubernetes/legos/k8s_get_all_evicted_pods_from_namespace/k8s_get_all_evicted_pods_from_namespace.py
@@ -0,0 +1,67 @@
+#
+# Copyright (c) 2022 unSkript.com
+# All rights reserved.
+#
+
+from pydantic import BaseModel, Field
+from typing import Optional, Tuple
+import pprint
+import json
+
+class InputSchema(BaseModel):
+    namespace: Optional[str] = Field(
+        default='',
+        title='Namespace',
+        description='k8s Namespace')
+
+
+def k8s_get_all_evicted_pods_from_namespace_printer(output):
+    if output is None:
+        return
+    pprint.pprint(output)
+
+
+def k8s_get_all_evicted_pods_from_namespace(handle, namespace: str = "") -> Tuple:
+    """k8s_kubectl_command executes the given kubectl command on the pod
+
+        :type handle: object
+        :param handle: Object returned from the Task validate method
+        
+        :type namespace: str
+        :param namespace: k8s namespace.
+
+        :rtype: Tuple of execution result and list of evicted pods
+    """
+    if handle.client_side_validation != True:
+        print(f"K8S Connector is invalid: {handle}")
+        return str()
+
+    kubectl_command = "kubectl get pods --all-namespaces -o json | grep Evicted"
+    if namespace:
+        kubectl_command = "kubectl get pods -n " + namespace + " -o json | grep Evicted"
+
+    response = handle.run_native_cmd(kubectl_command)
+    if response is None or hasattr(response, "stderr") is False or response.stderr is None:
+        print(
+            f"Error while executing command ({kubectl_command}): {response.stderr}")
+        return str()
+
+    result = []
+    try:
+        pod_details = json.loads(response.stdout)
+        for k, v in pod_details.items():
+            if "items" in k:
+                for i in v:
+                    pod_dict = {}
+                    pod_dict["pod_name"] = i["metadata"]["name"]
+                    pod_dict["namespace"] = i["metadata"]["namespace"]
+                    result.append(pod_dict)
+    except Exception as e:
+        pass
+    
+    execution_flag = False
+    if len(result) > 0:
+        execution_flag = True
+    output = (execution_flag, result)
+    return output
+    
