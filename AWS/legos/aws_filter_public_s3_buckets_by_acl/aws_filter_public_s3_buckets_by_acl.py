09572163e4c2a1372ed4d8e6d8ec7210c6138727
shloka-bhalgat-unskript <shloka@unskript.com>
2023-01-06 18:11:37 +0530

    Modified lego to return list of objects

--- AWS/legos/aws_filter_public_s3_buckets_by_acl/aws_filter_public_s3_buckets_by_acl.py
+++ AWS/legos/aws_filter_public_s3_buckets_by_acl/aws_filter_public_s3_buckets_by_acl.py
@@ -21,22 +21,19 @@ def aws_get_public_s3_buckets_printer(output):
         return
     pprint.pprint(output)
 
-def check_publicly_accessible_buckets(s3Client,bucket,permission):
-    result = []
-    public_buckets = []
+def check_publicly_accessible_buckets(s3Client,b,permission):
     public_check = ["http://acs.amazonaws.com/groups/global/AuthenticatedUsers",
                    "http://acs.amazonaws.com/groups/global/AllUsers"]
-    for b in bucket['bucket']:
-        try:
-            res = s3Client.get_bucket_acl(Bucket=b)
-            for perm in permission:
-                for grant in res["Grants"]:
-                    if 'Permission' in grant.keys() and perm == grant["Permission"]:
-                        if 'URI' in grant["Grantee"] and grant["Grantee"]["URI"] in public_check:
-                            if b not in public_buckets:
-                                public_buckets.append(b)
-        except Exception as e:
-            continue
+    public_buckets = False
+    try:
+        res = s3Client.get_bucket_acl(Bucket=b)
+        for perm in permission:
+            for grant in res["Grants"]:
+                if 'Permission' in grant.keys() and perm == grant["Permission"]:
+                    if 'URI' in grant["Grantee"] and grant["Grantee"]["URI"] in public_check:
+                        public_buckets = True
+    except Exception as e:
+        pass
     return public_buckets
 
 def aws_get_public_s3_buckets(handle, region: str=None) -> Tuple:
@@ -55,27 +52,26 @@ def aws_get_public_s3_buckets(handle, region: str=None) -> Tuple:
     result = []
     all_buckets = []
     all_regions = [region]
-    if region is None or not region:
+    if region is None or len(region)==0:
         all_regions = aws_list_all_regions(handle=handle)
     try:
         for r in all_regions:
-            all_buckets_dict = {}
             s3Client = handle.client('s3',region_name=r)
-            output =aws_get_s3_buckets(handle=handle, region=r)
-            if len(output)!=0:
-                all_buckets_dict["region"]=r
-                all_buckets_dict["bucket"]=output
-                all_buckets.append(all_buckets_dict)
+            output = aws_get_s3_buckets(handle=handle, region=r)
+            if len(output)!= 0:
+                for o in output:
+                    all_buckets_dict = {}
+                    all_buckets_dict["region"]=r
+                    all_buckets_dict["bucket"]=o
+                    all_buckets.append(all_buckets_dict)
     except Exception as e:
         pass
     for bucket in all_buckets:
         try:
-            b_dict = {}
             s3Client = handle.client('s3',region_name= bucket['region'])
-            o = check_publicly_accessible_buckets(s3Client,bucket, permission)
-            b_dict["region"] = bucket['region']
-            b_dict["bucket"] = o
-            result = [*result, b_dict]
+            flag = check_publicly_accessible_buckets(s3Client,bucket['bucket'], permission)
+            if flag:
+                result.append(bucket)
         except Exception as e:
             pass
     execution_flag = False
