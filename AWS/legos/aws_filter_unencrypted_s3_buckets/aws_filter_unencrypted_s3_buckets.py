a67883ed912dd63451cb3f64e267e6cfe0b9c314
Harshal Bangre <harsh@Harshal.localdomain>
2023-01-27 18:06:31 +0530

    Check Action: Filter AWS Unencrypted S3 Buckets

--- AWS/legos/aws_filter_unencrypted_s3_buckets/aws_filter_unencrypted_s3_buckets.py
+++ AWS/legos/aws_filter_unencrypted_s3_buckets/aws_filter_unencrypted_s3_buckets.py
@@ -3,7 +3,8 @@
 ##  All rights reserved.
 ##
 from pydantic import BaseModel, Field
-from typing import Tuple, Optional
+from typing import Optional
+from unskript.legos.utils import CheckOutput, CheckOutputStatus
 from unskript.legos.aws.aws_list_all_regions.aws_list_all_regions import aws_list_all_regions
 from botocore.exceptions import ClientError
 import pprint
@@ -19,19 +20,20 @@ class InputSchema(BaseModel):
 def aws_filter_unencrypted_s3_buckets_printer(output):
     if output is None:
         return
-    pprint.pprint(output)
+    pprint.pprint(output.json())
 
 
-def aws_filter_unencrypted_s3_buckets(handle, region: str = "") -> Tuple:
-    """aws_filter_unencrypted_s3_buckets List of unencrypted bucket name .
+@beartype
+def aws_filter_unencrypted_s3_buckets(handle, region: str = "") -> CheckOutput:
+    """aws_filter_unencrypted_s3_buckets List of unencrypted S3 bucket name .
 
         :type handle: object
         :param handle: Object returned from task.validate(...).
-        
+
         :type region: string
         :param region: Filter S3 buckets.
 
-        :rtype: Tuple with execution result and unencrypted bucket name.
+        :rtype: CheckOutput with status result and list of unencrypted S3 bucket name.
     """
     result = []
     all_regions = [region]
@@ -54,9 +56,7 @@ def aws_filter_unencrypted_s3_buckets(handle, region: str = "") -> Tuple:
         except Exception as error:
             pass
 
-    execution_flag = False
-    if len(result) > 0:
-        execution_flag = True
-    output = (execution_flag, result)
-    return output
+    return CheckOutput(status=CheckOutputStatus.SUCCESS,
+                       objects=result,
+                       error=str(""))
 
