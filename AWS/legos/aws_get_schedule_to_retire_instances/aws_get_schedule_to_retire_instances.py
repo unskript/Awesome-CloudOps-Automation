eed6322813b78747946323392253c5ef83d64a80
shloka-bhalgat-unskript <shloka@unskript.com>
2023-01-31 16:28:30 +0530

    Check Lego: Get Instances Scheduled to Retire

--- AWS/legos/aws_get_schedule_to_retire_instances/aws_get_schedule_to_retire_instances.py
+++ AWS/legos/aws_get_schedule_to_retire_instances/aws_get_schedule_to_retire_instances.py
@@ -6,6 +6,7 @@
 from pydantic import BaseModel, Field
 from typing import List, Tuple, Optional
 from unskript.legos.aws.aws_list_all_regions.aws_list_all_regions import aws_list_all_regions
+from unskript.legos.utils import CheckOutput, CheckOutputStatus
 from unskript.legos.aws.aws_filter_all_ec2_instances.aws_filter_all_ec2_instances import aws_filter_ec2_instances
 import pprint
 
@@ -21,15 +22,18 @@ class InputSchema(BaseModel):
 def aws_get_schedule_to_retire_instances_printer(output):
     if output is None:
         return
-    pprint.pprint(output)
+    if isinstance(output, CheckOutput):
+        pprint.pprint(output.json())
+    else:
+        pprint.pprint(output)
 
-def aws_get_schedule_to_retire_instances( handle, region: str=None) ->Tuple:
+def aws_get_schedule_to_retire_instances( handle, region: str=None) ->CheckOutput:
     """aws_get_schedule_to_retire_instances Returns a tuple of instances scheduled to retire.
 
         :type region: string
         :param region: Used to filter the volume for specific region.
 
-        :rtype: Tuple with the execution result and list of instances scheduled to retire
+        :rtype: Object with status, list of instances scheduled to retire, and errors
     """
     retiring_instances = {} 
     result = [] 
@@ -37,8 +41,8 @@ def aws_get_schedule_to_retire_instances( handle, region: str=None) ->Tuple:
     all_regions = [region] 
     if region is None or not region:
          all_regions = aws_list_all_regions(handle=handle) 
-    try: 
-        for r in all_regions: 
+    for r in all_regions:
+        try: 
             ec2client = handle.client('ec2', region_name=r) 
             output = aws_filter_ec2_instances(handle=handle,region=r)
             if len(output)!=0: 
@@ -47,9 +51,9 @@ def aws_get_schedule_to_retire_instances( handle, region: str=None) ->Tuple:
                     all_instances_dict["region"]=r 
                     all_instances_dict["instance"]=o 
                     all_instances.append(all_instances_dict) 
-    except Exception as error: 
-        pass 
-    for each_instance in all_instances: 
+        except Exception as error:
+            pass
+    for each_instance in all_instances:
         try: 
             ec2client = handle.client('ec2', region_name=each_instance['region'])
             response = ec2client.describe_instance_status(Filters=[{'Name': 'event.code','Values': ['instance-retirement']}],InstanceIds=each_instance['instance']) 
@@ -59,8 +63,11 @@ def aws_get_schedule_to_retire_instances( handle, region: str=None) ->Tuple:
                 result.append(retiring_instances) 
         except Exception as error: 
             pass 
-    execution_flag = False 
-    if len(result) > 0:
-        execution_flag = True 
-    output = (execution_flag, result) 
-    return output
+    if len(result)!=0:
+        return CheckOutput(status=CheckOutputStatus.FAILED,
+                   objects=result,
+                   error=str(""))
+    else:
+        return CheckOutput(status=CheckOutputStatus.SUCCESS,
+                   objects=result,
+                   error=str(""))
