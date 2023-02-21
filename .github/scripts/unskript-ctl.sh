47be4f4a7774b8f04c7027094dcccb3d1d6f252d
Jayasimha Raghavan <87547684+jayasimha-raghavan-unskript@users.noreply.github.com>
2023-02-20 16:03:54 -0800

    Moved unskript-ctl from unskript repo to Awesome repo (#233)
    Co-authored-by: Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>

---
+++ .github/scripts/unskript-ctl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# unSkript Control Script 
+#     This script can be used to list all available runbook
+#     and Run the runbook
+
+/usr/bin/env python /usr/local/bin/unskript-client.py "$@"
