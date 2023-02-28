efc2a86fbb9bc28cc4564ffb20d7e1d8a039afd4
shloka-bhalgat-unskript <shloka@unskript.com>
2023-02-28 23:25:26 +0530

    Action: Github Get Team

---
+++ Github/legos/github_get_team/github_get_team.py
@@ -0,0 +1,54 @@
+
+from typing import Optional, List
+from github import GithubException
+from pydantic import BaseModel, Field
+import pprint
+
+class InputSchema(BaseModel):
+    organization_name: Optional[str] = Field(
+        description='Github Organization Name', 
+        title='Organization Name'
+    )
+    team_name: Optional[str] = Field(
+        description='Team name in a GitHub Organization', 
+        title='Team name'
+    )
+
+
+def github_get_team_printer(output):
+    if not output:
+        return
+    pprint.pprint(output)
+
+def github_get_team(handle, organization_name:str, team_name:str) -> List:
+    """github_get_all_branches returns 100 open github branches for a user.
+
+        :type handle: object
+        :param handle: Object returned from task.validate(...).
+
+        :type organization_name: string
+        :param organization_name: Organization name Eg: "infosec"
+
+        :type team_name: string
+        :param team_name: Team name. Eg: "backend"
+
+        :rtype: List of objects of a team and its id in an organization
+    """
+    result = []
+    try:
+        organization = handle.get_organization(organization_name)
+        team = organization.get_team_by_slug(team_name)
+        team_details = {}
+        team_details["team_name"] = team.name
+        team_details["team_id"] = team.id
+        result.append(team_details)
+    except GithubException as e:
+        if e.status == 403:
+            return [f"You need admin access"]
+        if e.status == 404:
+            return [f"No such organization or repository"]
+        else:
+            return [e.data]
+    return result
+
+
