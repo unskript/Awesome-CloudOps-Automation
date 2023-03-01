7cbd5982fbc44256cb84a2cb1a26ce75622ffd62
shloka-bhalgat-unskript <shloka@unskript.com>
2023-03-01 18:56:07 +0530

    Action: Github Create Team

---
+++ Github/legos/github_create_team/github_create_team.py
@@ -0,0 +1,74 @@
+
+from typing import Optional, List
+from github import GithubException
+from pydantic import BaseModel, Field
+from unskript.enums.github_team_privacy_enums import GithubTeamPrivacy
+import pprint
+
+class InputSchema(BaseModel):
+    team_name: str = Field(
+     description='Team name. Eg:"backend"', 
+     title='Team Name'
+    )
+    description: Optional[str] = Field(
+        '', 
+        description='Description of the new team.', 
+        title='Description'
+    )
+    privacy: Optional[GithubTeamPrivacy] = Field(
+        description='Privacy type to be given to the team. "secret" - only visible to organization owners and members of this team, "closed"- visible to all members of this organization. By default type "secret" will be considered. ', 
+        title='Privacy'
+    )
+    organization_name: str = Field(
+       description='Github Organization Name. Eg: "infosecorg"', 
+       title='Organization Name'
+    )
+    repositories: List = Field(
+        description='List of the GitHub repositories to add to the new team. Eg: ["repo1","repo2"]',
+        title='repositories',
+    )
+
+
+
+def github_create_team_printer(output):
+    if not output:
+        return
+    pprint.pprint(output)
+
+def github_create_team(handle, organization_name:str, team_name:str,repositories:list, privacy:GithubTeamPrivacy=None, description:str="") -> List:
+    """github_create_team returns details of newly created team.
+
+        :type handle: object
+        :param handle: Object returned from task.validate(...).
+
+        :type organization_name: string
+        :param organization_name: Organization name Eg: "infosec"
+
+        :type team_name: string
+        :param team_name: Team name. Eg: "backend"
+
+        :type description: string
+        :param description: Description of the new team.
+
+        :type repositories: string
+        :param repositories: List of the GitHub repositories to add to the new team. Eg: ["repo1","repo2"]
+
+        :type privacy: Enum
+        :param privacy: Privacy type to be given to the team. "secret" - only visible to organization owners and members of this team, "closed"- visible to all members of this organization. By default type "secret" will be considered. 
+
+        :rtype: List of details of newly created team
+    """
+    result = []
+    repo_names =[]
+    list_of_repos = ''
+    if privacy is None or len(privacy)==0:
+        privacy_settings = "secret"
+    organization = handle.get_organization(organization_name)
+    for repo in repositories:
+        list_of_repos  = organization.get_repo(repo)
+    repo_names.append(list_of_repos)
+    try:
+        result = organization.create_team(name=team_name, repo_names=repo_names, privacy=privacy_settings, description=description)
+    except GithubException as e:
+        return [e.data]
+    return result
