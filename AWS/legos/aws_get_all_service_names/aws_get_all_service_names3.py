bf27553c80a5a15828ca25afae688ee351d49d38
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-03-07 15:36:48 -0500

    updqte get all service names imports

--- AWS/legos/aws_get_all_service_names/aws_get_all_service_names3.py
+++ AWS/legos/aws_get_all_service_names/aws_get_all_service_names3.py
@@ -7,25 +7,22 @@ from pydantic import BaseModel, Field
 class InputSchema(BaseModel):
     region: str = Field(..., description='The AWS Regiob', title='region')
 
-##
-##  Copyright (c) 2023 unSkript, Inc
-##  All rights reserved.
-##
+from unskript.connectors.aws import aws_get_paginator
 from pydantic import BaseModel, Field
 from typing import List, Dict
-import pprint
-from unskript.connectors.aws import aws_get_paginator
 
 from beartype import beartype
 @beartype
-def aws_get_all_service_names2_printer(output):
+@beartype
+def aws_get_all_service_names3_printer(output):
     if output is None:
         return
     pprint.pprint(output)
 
 
 @beartype
-def aws_get_all_service_names2(handle, region:str) -> List:
+@beartype
+def aws_get_all_service_names3(handle, region:str) -> List:
     sqClient = handle.client('service-quotas',region_name=region)
     resPaginate = aws_get_paginator(sqClient,'list_services','Services',PaginationConfig={
         'MaxItems': 1000,
@@ -34,5 +31,7 @@ def aws_get_all_service_names2(handle, region:str) -> List:
 
     #res = sqClient.list_services(MaxResults = 100)
     return resPaginate
+
+
 
 
