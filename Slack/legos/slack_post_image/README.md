7826b2e3fabdd0a1cfc0ee25bcda8757cd72a675
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2023-03-17 14:36:46 -0400

    2 legos: post slack image, comment on Github issue

---
+++ Slack/legos/slack_post_image/README.md
@@ -0,0 +1,26 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>Post Slack Message with an Image</h1>
+
+## Description
+This Lego Post Slack Message with an Image and gives a message sent status.
+
+
+## Lego Details
+
+    slack_post_image(handle: object, channel: str, message: str, image: str)
+
+        handle: Object of type unSkript AWS Connector
+        channel: Name of slack channel.
+        message: Message sent to channel.
+        image: File Name of the image to be sent in the message.
+
+## Lego Input
+This Lego take four inputs handle, channel, message and image.
+
+## Lego Output
+Here is a sample output.
+<img src="./1.jpg">
+<img src="./2.jpg">
+
+## See it in Action
+You can see this Lego in action following this link [unSkript Live](https://unskript.com)
---
+++ Slack/legos/slack_post_image/slack_post_image.py
@@ -0,0 +1,54 @@
+##
+# Copyright (c) 2021 unSkript, Inc
+# All rights reserved.
+##
+
+import pprint
+
+from pydantic import BaseModel, Field
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+pp = pprint.PrettyPrinter(indent=2)
+
+from beartype import beartype
+
+class InputSchema(BaseModel):
+    channel: str = Field(
+        title='Channel',
+        description='Name of slack channel.')
+    message: str = Field(
+        title='message',
+        description='Message for slack channel.')
+    image: str = Field(
+        title='image',
+        description='Path to image to be sent.')
+
+@beartype
+def slack_post_image_printer(output):
+    if output is not None:
+        pprint.pprint(output)
+    else:
+        return
+
+
+@beartype
+def slack_post_image(
+        handle: WebClient,
+        channel: str,
+        message:str,
+        image: str) -> str:
+
+    try:
+        result = handle.files_upload(
+            channels = channel,
+            initial_comment=message,
+            file=image
+    )
+        return f"Successfully Sent Message on Channel: #{channel}"
+
+    except Exception as e:
+        print("\n\n")
+        pp.pprint(
+            f"Failed sending message to slack channel {channel}, Error: {e.__str__()}")
+        return f"Unable to send message on {channel}"
