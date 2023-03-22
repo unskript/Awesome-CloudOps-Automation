cdaad5f27e7f333bbdb41651064d49fd986e9b2c
ssalvi <sagar.salvi@unskript.com>
2023-03-22 17:22:28 +0530

    Datadog list metrics lego

---
+++ Datadog/legos/datadog_list_metrics/datadog_list_metrics.py
@@ -0,0 +1,36 @@
+##
+##  Copyright (c) 2021 unSkript, Inc
+##  All rights reserved.
+##
+from pydantic import BaseModel, Field
+from datadog_api_client.v1.api.metrics_api import MetricsApi
+from datadog_api_client import ApiClient
+from typing import Dict, Optional
+import pprint
+
+class InputSchema(BaseModel):
+    query: Optional[str] = Field(
+        "",
+        title='Query',
+        description='Query string to list metrics upon. Can optionally be prefixed with ``metrics:``. By default all metrics are returned')
+
+def datadog_list_metrics_printer(output):
+    if output is None:
+        return
+    pprint.pprint(output)
+
+def datadog_list_metrics(handle, query: str = "") -> Dict:
+    """datadog_list_metrics lists metrics from the last 24 hours in Datadog.
+
+        :type name: str
+        :param query: Query string to list metrics upon. Can optionally be prefixed with ``metrics:``.
+
+        :rtype: A Dict containing the queried metrics
+    """
+    try:
+        with ApiClient(handle) as api_client:
+            api_instance = MetricsApi(api_client)
+            metrics = api_instance.list_metrics(q=query)
+    except Exception as e:
+        raise e
+    return metrics.to_dict()
