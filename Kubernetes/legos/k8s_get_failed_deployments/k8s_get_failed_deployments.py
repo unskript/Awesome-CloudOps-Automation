18677f866fb7e2725b128e13aca4574a068f91fc
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-03-31 10:54:41 -0700

    Second ChatGPT lego added

---
+++ Kubernetes/legos/k8s_get_failed_deployments/k8s_get_failed_deployments.py
@@ -0,0 +1,42 @@
+#
+# Copyright (c) 2023 unSkript.com
+# All rights reserved.
+#
+from typing import Tuple
+from kubernetes import client
+from kubernetes.client.rest import ApiException
+from pydantic import BaseModel, Field
+
+
+class InputSchema(BaseModel):
+    pass
+
+def k8s_get_failed_deployments_printer(output):
+    if output is None:
+        return 
+    
+    print(output)
+
+
+def k8s_get_failed_deployments(handle) -> Tuple:
+    """k8s_get_failed_deployments Returns the list of all failed deployments across all namespaces
+
+    :type handle: Object
+    :param handle: Object returned from task.validate(...) function
+
+    :rtype: Tuple of the result
+    """
+    if handle.client_side_validation != True:
+        raise Exception(f"K8S Connector is invalid {handle}")
+
+    apps_client = client.AppsV1Api(api_client=handle)
+    deployments = apps_client.list_deployment_for_all_namespaces().items 
+    retval = {} 
+    for deployment in deployments:
+        if deployment.status.available_replicas == 0:
+            retval.update(deployment)
+    
+    if not retval:
+        return (False, [retval])
+    
+    return (True, [])
