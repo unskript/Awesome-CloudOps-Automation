cd21c9e8816bb0ad654546f3f3cced0233d45aa4
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-04-01 14:22:02 -0700

    PR Comments

--- Kubernetes/legos/k8s_get_service_with_no_associated_endpoints/k8s_get_service_with_no_associated_endpoints.py
+++ Kubernetes/legos/k8s_get_service_with_no_associated_endpoints/k8s_get_service_with_no_associated_endpoints.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2023 unSkript.com
 # All rights reserved.
 #
+import pprint 
+
 from typing import Tuple
 from kubernetes import client
 from kubernetes.client.rest import ApiException
@@ -9,7 +11,11 @@ from pydantic import BaseModel, Field
 
 
 class InputSchema(BaseModel):
-    pass
+    namespace: str = Field(
+        default = "",
+        title = "K8S Namespace",
+        description = "Kubernetes Namespace Where the Service exists"
+    )
 
 def k8s_get_service_with_no_associated_endpoints_printer(output):
     if output is None:
@@ -17,12 +23,15 @@ def k8s_get_service_with_no_associated_endpoints_printer(output):
     
     print(output)
 
-def k8s_get_service_with_no_associated_endpoints(handle) -> Tuple:
+def k8s_get_service_with_no_associated_endpoints(handle, namespace: str = "") -> Tuple:
     """k8s_get_service_with_no_associated_endpoints This function returns Services that
        do not have any associated endpoints.
 
        :type handle: Object
        :param handle: Object returned from the task.validate(...) function
+
+       :type namespace: str
+       :param namespace: String, K8S Namespace as python string 
 
        :rtype: Tuple Result in tuple format.
     """
@@ -30,12 +39,22 @@ def k8s_get_service_with_no_associated_endpoints(handle) -> Tuple:
         raise Exception(f"K8S Connector is invalid {handle}")
     
     v1 = client.CoreV1Api(api_client=handle)
+    
     services = v1.list_service_for_all_namespaces().items
+    services_to_check = services
+    if namespace != "":
+        for s in services:
+            if s.metadata.namespace == namespace:
+                services_to_check = s
+                break
+        
     retval = []
-    for service in services:
-        endpoints = v1.list_namespaced_endpoints(namespace=service.metadata.namespace, field_selector=f"metadata.name={service.metadata.name}").items
-        if not endpoints:
-            retval.append(service)
+    for service in services_to_check:
+        ep = v1.read_namespaced_endpoints(service.metadata.name, service.metadata.namespace)
+        if not ep.subsets:
+            print(f"SVC NAME: {service.metadata.name} SVC NAMESPACE: {service.metadata.namespace}")
+            pprint.pprint(ep)
+            retval.append({"name": service.metadata.name, "namespace": service.metadata.namespace})
 
     if retval:
         return (False, retval)
