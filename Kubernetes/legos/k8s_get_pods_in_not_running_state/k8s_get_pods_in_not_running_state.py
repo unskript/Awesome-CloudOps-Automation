41a9b465aad6fe4f3d5fe91facb8bd4798c0a3fe
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-04-03 08:16:42 -0700

    PR Comments

--- Kubernetes/legos/k8s_get_pods_in_not_running_state/k8s_get_pods_in_not_running_state.py
+++ Kubernetes/legos/k8s_get_pods_in_not_running_state/k8s_get_pods_in_not_running_state.py
@@ -9,7 +9,11 @@ from pydantic import BaseModel, Field
 
 
 class InputSchema(BaseModel):
-    pass
+    namespace: str = Field(
+        '',
+        description='K8S Namespace',
+        title='K8S Namespace'
+    )
 
 def k8s_get_pods_in_not_running_state(output):
     if output is None:
@@ -18,7 +22,7 @@ def k8s_get_pods_in_not_running_state(output):
     print(output)
 
 
-def k8s_get_pods_in_not_running_state(handle) -> Tuple:
+def k8s_get_pods_in_not_running_state(handle, namespace: str = '') -> Tuple:
     """k8s_get_pods_in_not_running_state This check function uses the handle's native command
        method to execute a pre-defined kubectl command and returns the output of list of pods
        not in running state.
@@ -31,12 +35,15 @@ def k8s_get_pods_in_not_running_state(handle) -> Tuple:
     if handle.client_side_validation != True:
         raise Exception(f"K8S Connector is invalid {handle}")
 
-    kubectl_command = "kubectl get pods --all-namespaces --field-selector=status.phase!=Running -o jsonpath=\"{.items[*]['metadata.name', 'metadata.namespace']}\""
+    if not namespace:
+        kubectl_command = "kubectl get pods --all-namespaces --field-selector=status.phase!=Running -o jsonpath=\"{.items[*]['metadata.name', 'metadata.namespace']}\""
+    else:
+        kubectl_command = f"kubectl get pods -n {namespace}" +  " --field-selector=status.phase!=Running -o jsonpath=\"{.items[*]['metadata.name', 'metadata.namespace']}\""
     result = handle.run_native_cmd(kubectl_command)
     if result.stderr:
         raise Exception(f"Error occurred while executing command {result.stderr}")
     
     if result.stdout:
-        return (False, [result.stdout.split()])
+        return (False, [{'name': result.stdout.split()[0], 'namespace': result.stdout.split()[-1]}])
     
     return (True, [])
