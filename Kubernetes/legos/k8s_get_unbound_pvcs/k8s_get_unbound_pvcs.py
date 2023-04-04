0db9fd71832d80a78f1d8294b491979c6d2bd80b
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-04-03 19:23:52 -0700

    PR Comments

--- Kubernetes/legos/k8s_get_unbound_pvcs/k8s_get_unbound_pvcs.py
+++ Kubernetes/legos/k8s_get_unbound_pvcs/k8s_get_unbound_pvcs.py
@@ -9,7 +9,11 @@ from pydantic import BaseModel, Field
 
 
 class InputSchema(BaseModel):
-    pass
+    namespace: str = Field (
+        '',
+        description='K8S Namespace',
+        title="K8S Namespace"
+    )
 
 def k8s_get_unbound_pvcs_printer(output):
     if output is None:
@@ -17,11 +21,14 @@ def k8s_get_unbound_pvcs_printer(output):
     
     print(output)
 
-def k8s_get_unbound_pvcs(handle) -> Tuple:
+def k8s_get_unbound_pvcs(handle, namespace:str = '') -> Tuple:
     """k8s_get_unbound_pvcs This function all unbound PVCS and returns them back
 
        :type handle: Object
        :param handle: Object returned from the task.validate(...) function
+
+       :type namespace: str
+       :param namespace: Kubernetes Namespace 
 
        :rtype: Tuple Result in tuple format.  
     """
@@ -31,13 +38,35 @@ def k8s_get_unbound_pvcs(handle) -> Tuple:
     v1 = client.CoreV1Api(api_client=handle)
 
     # Get all PVCs in the cluster
-    pvc_list = v1.list_persistent_volume_claim_for_all_namespaces().items
+    if not namespace:
+        pvc_list = v1.list_persistent_volume_claim_for_all_namespaces().items
+        pod_list = v1.list_pod_for_all_namespaces().items
+    else:
+        pvc_list = v1.list_namespaced_persistent_volume_claim(namespace).items
+        pod_list = v1.list_namespaced_pod(namespace).items
+
     retval = []
+    matched_volume = {}
+    list_all_volumes = {}
     # Iterate through each PVC and check its status
     for pvc in pvc_list:
         # Check if the PVC is bound to a volume
-        if pvc.status.phase != "Bound":
-            retval.append(pvc)
+        for pod in pod_list:
+            found = False
+            for volume in pod.spec.volumes:
+                if volume.persistent_volume_claim != None:
+                    if volume.persistent_volume_claim.claim_name == pvc.metadata.name:
+                        found = True
+                        matched_volume[pvc.metadata.name] = pvc.metadata.namespace
+                        list_all_volumes[pvc.metadata.name] = pvc.metadata.namespace
+                        break
+                    else:
+                        list_all_volumes[pvc.metadata.name] = pvc.metadata.namespace
+    
+    if len(matched_volume) != len(list_all_volumes):
+       for k,v in list_all_volumes.items():
+           if k not in matched_volume.keys():
+               retval.append({'name': k, 'namespace': v})
 
     if retval:
         return (False, retval)
