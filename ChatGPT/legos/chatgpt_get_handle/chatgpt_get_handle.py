38c6611f5e3333e0275060a8ccfc34df94d15421
shloka-bhalgat-unskript <110628398+shloka-bhalgat-unskript@users.noreply.github.com>
2023-04-05 22:35:26 +0530

    Action: ChatGPT Get Handle (#401)

---
+++ ChatGPT/legos/chatgpt_get_handle/chatgpt_get_handle.py
@@ -0,0 +1,17 @@
+##
+##  Copyright (c) 2023 unSkript, Inc
+##  All rights reserved.
+##
+from pydantic import BaseModel
+
+
+class InputSchema(BaseModel):
+    pass
+
+
+def chatgpt_get_handle(handle):
+    """chatgpt_get_handle returns the nomad handle.
+
+          :rtype: ChatGPT handle.
+    """
+    return handle
