0f603229a6283851a743ff25142eaa6ac1d5a600
Harshal Bangre <harsh@Harshal.localdomain>
2023-04-12 18:50:46 +0530

    [Action]: AWS Delete Load Balancer

---
+++ AWS/legos/aws_delete_load_balancer/README.md
@@ -0,0 +1,25 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>AWS Delete Load Balancer</h1>
+
+## Description
+This Lego delete AWS load balancer.
+
+
+## Lego Details
+
+    aws_delete_load_balancer(handle, region: str, elb_arn: str)
+
+        handle: Object of type unSkript AWS Connector.
+        elb_arn:  load balancer ARNs.
+        region: AWS Region of the bucket.
+
+## Lego Input
+This Lego take three inputs handle, elb_arn and region.
+
+## Lego Output
+Here is a sample output.
+<img src="./1.png">
+
+## See it in Action
+
+You can see this Lego in action following this link [unSkript Live](https://us.app.unskript.io)
---
+++ AWS/legos/aws_delete_load_balancer/aws_delete_load_balancer.py
@@ -0,0 +1,42 @@
+##
+# Copyright (c) 2021 unSkript, Inc
+# All rights reserved.
+##
+from pydantic import BaseModel, Field
+from typing import Optional, Dict
+import pprint
+from unskript.legos.aws.aws_get_handle.aws_get_handle import Session
+
+
+class InputSchema(BaseModel):
+    elb_arn: str = Field(
+        title='Load Balancer ARNs',
+        description='load balancer ARNs.')
+    region: str = Field(
+        title='Region',
+        description='AWS Region of the bucket.')
+
+
+def aws_delete_load_balancer_printer(output):
+    if output is None:
+        return
+    pprint.pprint(output)
+
+
+def aws_delete_load_balancer(handle, region: str, elb_arn: str) -> Dict:
+    """aws_delete_load_balancer dict of loadbalancers info.
+
+        :type region: string
+        :param region: AWS Region.
+
+        :type elb_arn: string
+        :param elb_arn: load balancer ARNs.
+
+        :rtype: dict of load balancers info.
+    """
+    try:
+        ec2Client = handle.client('elbv2', region_name=region)
+        response = ec2Client.delete_load_balancer(LoadBalancerArn=elb_arn)
+        return response
+    except Exception as e:
+        raise Exception(e)
