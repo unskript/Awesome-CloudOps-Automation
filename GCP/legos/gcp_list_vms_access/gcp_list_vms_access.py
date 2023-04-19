40b84a21ac7b4ace779eddbe9ac7022bcd4bb865
Doug Sillars <doug.sillars@gmail.com>
2023-04-18 21:48:35 -0400

    created 2 more actions using chat GPT (#192)
    Co-authored-by: Doug Sillars <dougsillars@Dougs-MacBook-Air.local>

---
+++ GCP/legos/gcp_list_vms_access/gcp_list_vms_access.py
@@ -0,0 +1,45 @@
+##  Copyright (c) 2023 unSkript, Inc
+## Written by Doug Sillars & ChatGPT
+##  All rights reserved.
+##
+from pydantic import BaseModel, Field
+import pprint
+from typing import List,Any, Dict
+from google.cloud import compute
+
+from beartype import beartype
+
+
+class InputSchema(BaseModel):
+    project: str = Field(
+        title='Project Name',
+        description='Name of the Google Cloud Project.')
+    zone: str = Field(
+        title='Zone',
+        description='Name of the Google Cloud Zone where the project is located.')
+
+
+
+@beartype
+def gcp_list_VMs_printer(output):
+    if len(output)==0:
+        print("There are no publicly readable buckets available")
+        return
+    print(output)
+
+
+@beartype
+def gcp_list_VMs(handle, project:str, zone:str) -> List:
+
+
+    compute_client = compute.InstancesClient(credentials=handle)
+    
+    vms = compute_client.list(project=project, zone=zone)
+    vm_list = []
+    for vm in vms:
+        vm_info = {}
+        vm_info['name'] = vm.name
+        vm_info['publicly_accessible'] = vm.can_ip_forward
+        vm_list.append(vm_info)
+
+    return(vm_list)
