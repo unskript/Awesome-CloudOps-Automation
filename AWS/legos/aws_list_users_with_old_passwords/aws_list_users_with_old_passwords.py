0a07544f39b21021fa27e09b4134a899c8c82330
Kaushik B A <kaushikbafeb@gmail.com>
2023-05-06 14:41:49 +0530

    edit-18

--- AWS/legos/aws_list_users_with_old_passwords/aws_list_users_with_old_passwords.py
+++ AWS/legos/aws_list_users_with_old_passwords/aws_list_users_with_old_passwords.py
@@ -1,20 +1,21 @@
 ##  Copyright (c) 2021 unSkript, Inc
 ##  All rights reserved.
 ##
-from pydantic import BaseModel, Field, SecretStr
-from typing import Dict, List
-from unskript.connectors.aws import aws_get_paginator
-from datetime import datetime, timezone, timedelta
-from dateutil.parser import parse
 import pprint
+from typing import List
+from datetime import datetime, timezone, timedelta
+from pydantic import BaseModel, Field
+from dateutil.parser import parse
+from unskript.connectors.aws import aws_get_paginator
 
 
 class InputSchema(BaseModel):
     threshold_days: int = Field(
         default = 120,
         title='Threshold (In days)',
-        description='(in days) The threshold to check the IAM user password older than the threshold.')
-    
+        description=('(in days) The threshold to check the IAM user '
+                     'password older than the threshold.')
+                    )
 
 def aws_list_users_with_old_passwords_printer(output):
     if output is None:
@@ -29,7 +30,8 @@ def aws_list_users_with_old_passwords(handle, threshold_days: int = 120) -> List
         :param handle: Object returned from Task Validate
 
         :type threshold_days: int
-        :param threshold_days: (in days) The threshold to check the IAM user password older than the threshold.
+        :param threshold_days: (in days) The threshold to check the IAM user
+        password older than the threshold.
 
         :rtype: Result List of all IAM users
     """
@@ -41,14 +43,15 @@ def aws_list_users_with_old_passwords(handle, threshold_days: int = 120) -> List
         try:
             login_profile = client.get_login_profile(UserName=user['UserName'])
             if 'CreateDate' in login_profile['LoginProfile']:
-                password_last_changed = parse(str(login_profile['LoginProfile']['CreateDate'])).replace(tzinfo=timezone.utc)
+                password_last_changed = parse(
+                    str(login_profile['LoginProfile']['CreateDate'])
+                    ).replace(tzinfo=timezone.utc)
                 password_age = now - password_last_changed
                 if password_age > timedelta(days=threshold_days):
                     users_list.append(user['UserName'])
-        except Exception as e:
+        except Exception:
             pass
-        
+
     if len(users_list) != 0:
         return (False, users_list)
-    else:
-        return (True, None)
+    return (True, None)
