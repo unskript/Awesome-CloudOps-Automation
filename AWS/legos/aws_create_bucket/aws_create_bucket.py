4f9746e8d47fc94013d830acd8e5d7675bb1f5e4
kaushik-bandodkar-unskript <132424376+kaushik-bandodkar-unskript@users.noreply.github.com>
2023-05-17 23:24:27 +0530

    Edit 3 (#613)
    Co-authored-by: Kaushik B A <kaushikbafeb@gmail.com>
    Co-authored-by: amit-chandak-unskript <79171703+amit-chandak-unskript@users.noreply.github.com>

--- AWS/legos/aws_create_bucket/aws_create_bucket.py
+++ AWS/legos/aws_create_bucket/aws_create_bucket.py
@@ -14,8 +14,8 @@ class InputSchema(BaseModel):
         description='Name of the bucket to be created.')
     acl: str = Field(
         title='ACL',
-        description='The Canned ACL to apply to the bucket. Possible values: '
-                     'private, public-read, public-read-write, authenticated-read.')
+        description=('The Canned ACL to apply to the bucket. Possible values: '
+                       'private, public-read, public-read-write, authenticated-read.'))
     region: Optional[str] = Field(
         title='Region',
         description='AWS Region of the bucket.')
