289a43f34ca9f8f9f0bccb44d3de4bc192be1d81
shloka-bhalgat-unskript <shloka@unskript.com>
2023-07-27 12:40:38 +0530

    Made the MongoDB Cluster Health output prettier

--- Mongo/legos/mongodb_get_server_status/mongodb_get_server_status.py
+++ Mongo/legos/mongodb_get_server_status/mongodb_get_server_status.py
@@ -5,6 +5,7 @@
 import pprint
 from typing import Dict
 from pydantic import BaseModel
+from tabulate import tabulate
 
 
 class InputSchema(BaseModel):
@@ -14,8 +15,20 @@ class InputSchema(BaseModel):
 def mongodb_get_server_status_printer(output):
     if output is None:
         return
-    print("\n\n")
-    pprint.pprint(output)
+
+    essential_metrics = ['version', 'uptime', 'localTime', 'connections', 'extra_info', 'asserts', 'opcounters']
+    pretty_output = []
+
+    for metric in essential_metrics:
+        if metric in output:
+            if type(output[metric]) is dict:
+                for submetric, value in output[metric].items():
+                    pretty_output.append([f"{metric}.{submetric}", str(value)])
+            else:
+                pretty_output.append([metric, str(output[metric])])
+
+    print("\n")
+    print(tabulate(pretty_output, headers=['Metric', 'Value'], tablefmt='fancy_grid'))
     return output
 
 
