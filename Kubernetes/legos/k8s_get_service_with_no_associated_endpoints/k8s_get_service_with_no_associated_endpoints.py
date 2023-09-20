a3263ccb335b6c936e91a11d8a3ac0151c306ee3
shloka-bhalgat-unskript <shloka@unskript.com>
2023-09-20 15:52:44 +0530

    Updated check to accept namespace as a parameter

--- Kubernetes/legos/k8s_get_service_with_no_associated_endpoints/k8s_get_service_with_no_associated_endpoints.py
+++ Kubernetes/legos/k8s_get_service_with_no_associated_endpoints/k8s_get_service_with_no_associated_endpoints.py
@@ -2,25 +2,29 @@
 # Copyright (c) 2023 unSkript.com
 # All rights reserved.
 #
-import pprint 
-from typing import Tuple
+from typing import Tuple, Optional
 from pydantic import BaseModel, Field
 from kubernetes import client
+from tabulate import tabulate
 from kubernetes.client.rest import ApiException
 
 
 class InputSchema(BaseModel):
-    namespace: str = Field(
+    namespace:Optional[str] = Field(
         default = "",
         title = "K8S Namespace",
         description = "Kubernetes Namespace Where the Service exists"
     )
 
 def k8s_get_service_with_no_associated_endpoints_printer(output):
-    if output is None:
-        return
+    status, data = output
+    if status:
+        print("No services with missing endpoints found !")
+    else:
+        table_headers = ["Namespace", "Service Name"]
+        table_data = [(entry["namespace"], entry["name"]) for entry in data]
 
-    print(output)
+        print(tabulate(table_data, headers=table_headers, tablefmt = "grid"))
 
 def k8s_get_service_with_no_associated_endpoints(handle, namespace: str = "") -> Tuple:
     """k8s_get_service_with_no_associated_endpoints This function returns Services that
@@ -30,7 +34,7 @@ def k8s_get_service_with_no_associated_endpoints(handle, namespace: str = "") ->
        :param handle: Object returned from the task.validate(...) function
 
        :type namespace: str
-       :param namespace: String, K8S Namespace as python string 
+       :param namespace: String, K8S Namespace as python string
 
        :rtype: Tuple Result in tuple format.
     """
@@ -38,22 +42,24 @@ def k8s_get_service_with_no_associated_endpoints(handle, namespace: str = "") ->
         raise ApiException(f"K8S Connector is invalid {handle}")
 
     v1 = client.CoreV1Api(api_client=handle)
-    services = v1.list_service_for_all_namespaces().items
-    services_to_check = services
-    if namespace != "":
-        for s in services:
-            if s.metadata.namespace == namespace:
-                services_to_check = s
-                break
+
+    # List services based on namespace
+    if namespace:
+        services = v1.list_namespaced_service(namespace=namespace).items
+    else:
+        services = v1.list_service_for_all_namespaces().items
 
     retval = []
-    for service in services_to_check:
-        ep = v1.read_namespaced_endpoints(service.metadata.name, service.metadata.namespace)
-        if not ep.subsets:
-            pprint.pprint(ep)
-            retval.append({"name": service.metadata.name, "namespace": service.metadata.namespace})
+
+    for service in services:
+        try:
+            ep = v1.read_namespaced_endpoints(service.metadata.name, service.metadata.namespace)
+            if not ep.subsets:
+                retval.append({"name": service.metadata.name, "namespace": service.metadata.namespace})
+        except ApiException as e:
+            raise e
 
     if retval:
         return (False, retval)
 
-    return(True, [])
+    return(True, None)
