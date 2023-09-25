5c33bed7b95c48b55a613d3abe91186b19b3e1b7
Amit Chandak <amit@unskript.com>
2023-09-25 09:48:36 -0700

    Remove unwanted fiels
    Signed-off-by: Amit Chandak <amit@unskript.com>

--- read_bson.py
+++
@@ -1,8 +0,0 @@
-from bson import decode_all
-from bson.json_util import dumps
-
-with open('/Users/amit/Desktop/tenants.bson','rb') as f:
-    data = decode_all(f.read())
-
-with open("/Users/amit/Desktop/tenants.json", "w") as outfile:
-    outfile.write(dumps(data, indent=2))
--- temp.py
+++
@@ -1,8 +0,0 @@
-import threading
-
-def f(x=None, y=None):
-    print(x,y)
-
-my_dict = {'x':1, 'y':{'z':2, 'a':1}}
-t = threading.Thread(target=f, kwargs=my_dict)
-t.start()
