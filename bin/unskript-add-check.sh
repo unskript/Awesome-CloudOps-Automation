285a3e1543eb8595d7d8927ccafbffe07cbeddb0
Amit Chandak <amit@unskript.com>
2023-09-25 17:17:57 -0700

    Fix the unskript-add-check script
    Signed-off-by: Amit Chandak <amit@unskript.com>

--- bin/unskript-add-check.sh
+++ bin/unskript-add-check.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Add creds script
-#     This script can be used to add credentials.
+# Add check script
+#     This script can be used to create new checks.
 
-if test -f "/usr/local/bin/unskript-add-check.py"; then
-	cd /usr/local/bin
-	/usr/bin/env python /usr/local/bin/unskript-add-check.py "$@"
-else
-	TOPDIR=$(git rev-parse --show-toplevel)
-    	AWESOME_DIRECTORY=Awesome-CloudOps-Automation
-	/usr/bin/env python $TOPDIR/$AWESOME_DIRECTORY/unskript-ctl/unskript-add-check.py "$@"
-fi
+TOPDIR=$(git rev-parse --show-toplevel)
+AWESOME_DIRECTORY=Awesome-CloudOps-Automation
+/usr/bin/env python $TOPDIR/$AWESOME_DIRECTORY/unskript-ctl/unskript-add-check.py "$@"
