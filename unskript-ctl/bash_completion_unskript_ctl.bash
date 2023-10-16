116f44a76821977a7fa10b00dcedb7e7a19ed7d1
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-10-16 12:50:19 -0700

    Changes to bash completion script to not create file everytime

--- unskript-ctl/bash_completion_unskript_ctl.bash
+++ unskript-ctl/bash_completion_unskript_ctl.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 _unskript-client-completion() {
     local cur prev opts
     COMPREPLY=()
@@ -14,10 +13,12 @@ _unskript-client-completion() {
 
     if [ -n "$unskript_client_script" ]; then
         # Check if the script exists and save check names
-        /usr/bin/env python "$unskript_client_script" --save-check-names /tmp/checknames.txt
-        /usr/bin/env python "$unskript_client_script" -h > /tmp/allopts.txt
-        if [ -f "/tmp/checknames.txt" ]; then
-            opt2="$(cat /tmp/checknames.txt)"
+        if [ ! -f "/tmp/allopts.txt" ]; then
+             /usr/bin/env python "$unskript_client_script" -h > /tmp/allopts.txt
+        fi
+        if [ ! -f "/tmp/checknames.txt" ]; then
+            /usr/bin/env python "$unskript_client_script" --save-check-names /tmp/checknames.txt
+
         fi
     fi
     # Define options with each option on a separate line using newline characters
@@ -27,7 +28,7 @@ _unskript-client-completion() {
     case "${prev}" in
         -rr|--run-runbook)
             # Provide completion suggestions for runbook filenames
-            COMPREPLY=( $(compgen -f -- "${cur}") )
+            COMPREPLY=( $(compgen -f -- "${cur}" -o nospace) )
             return 0
             ;;
 
@@ -35,48 +36,59 @@ _unskript-client-completion() {
             case ${prev} in
                 -rc|--run-checks)
                     case ${cur} in 
-                        -f)
-                            COMPREPLY=( ${opt2} )
+                        --check)
+                            cur=${cur#--check}
+                            opt2="$(cat /tmp/checknames.txt)"
+                            COMPREPLY=( $(compgen -W "${opt2}" -o nospace) )
                             ;;
                         *)
-                            COMPREPLY=( $(compgen -W "--all | --type | --failed | --check" -- "${cur}") )
+                            COMPREPLY=( $(compgen -W "--all  --type  --failed  --check" -o nospace) )
                             ;;
                     esac
                     return 0
                     ;;
                 *)
-                    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                    COMPREPLY=( $(compgen -W "${opts}" "${cur}" -o nospace) )
                     ;;
+
             esac
             return 0
             ;;
 
         -lc|--list-checks)
             # Provide completion suggestions for list-checks options
-            COMPREPLY=( $(compgen -W "--all |  --type" -- "${cur}") )
+            COMPREPLY=( $(compgen -W "--all   --type" -- "${cur}" -o nospace) )
             return 0
             ;;
 
         -sa|--show-audit-trail)
             # Provide completion suggestions for show-audit-trail options
-            COMPREPLY=( $(compgen -W "--all |  --type |  --execution_id" -- "${cur}") )
+            COMPREPLY=( $(compgen -W "--all   --type   --execution_id" -- "${cur}" -o nospace) )
             return 0
             ;;
 
         -dl|--display-failed-logs)
             # Provide completion suggestions for display-failed-logs options
-            COMPREPLY=( $(compgen -W "--execution_id <EXECUTION_ID>" -- "${cur}") )
+            COMPREPLY=( $(compgen -W "--execution_id <EXECUTION_ID>" -- "${cur}" -o nospace) )
             return 0
             ;;
 
         -cc|--create-credentials)
             # Provide completion suggestions for create-credentials options
-            COMPREPLY=( $(compgen -W "--type creds_file_path" -- "${cur}") )
+            COMPREPLY=( $(compgen -W "--type creds_file_path" -- "${cur}" -o nospace) )
             return 0
             ;;
 
         *)  # Default: Provide completion suggestions for global options
-            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}" -o nospace) )
+            if [ "${prev}" = "--check" ];
+            then
+                # Try to autocomplete the checks
+		cur=${cur#--check}
+                opt2="$(grep -F ${cur} /tmp/checknames.txt)"
+		COMPREPLY=( $(compgen -W "${opt2}" -o nospace) )
+            else
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}" -o nospace) )
+            fi
             return 0
             ;;
     esac
