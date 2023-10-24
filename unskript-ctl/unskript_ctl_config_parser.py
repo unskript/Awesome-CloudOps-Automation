700103fb30807c64f5421bee59ac30ee7ed7e1f5
Amit Chandak <amit@unskript.com>
2023-10-24 15:09:56 -0700

    Add more debugs Audit files delete support

--- unskript-ctl/unskript_ctl_config_parser.py
+++ unskript-ctl/unskript_ctl_config_parser.py
@@ -24,6 +24,7 @@ from pathlib import Path
 #)
 UNSKRIPT_CTL_CONFIG_FILE="/etc/unskript/unskript_ctl_config.yaml"
 UNSKRIPT_CTL_BINARY="/usr/local/bin/unskript-ctl.sh"
+UNSKRIPT_EXECUTION_DIR="/unskript/data/execution/workspace/"
 
 # Job config related
 JOB_CONFIG_CHECKS_KEY_NAME = "checks"
@@ -34,6 +35,10 @@ JOB_CONFIG_NOTIFY_KEY_NAME = "notify"
 
 # Credential section related
 CREDENTIAL_CONFIG_SKIP_VALUE_FOR_ARGUMENTS = ["no-verify-certs", "no-verify-ssl", "use-ssl"]
+
+# Global section related
+GLOBAL_CONFIG_AUDIT_PERIOD_KEY_NAME = "audit_period"
+GLOBAL_DEFAULT_AUDIT_PERIOD = 90
 
 class bcolors:
     HEADER = '\033[95m'
@@ -49,11 +54,13 @@ class bcolors:
 class Job():
     def __init__(
             self,
+            job_name: str,
             checks: list[str],
             suites: list[str]=None,
             connectors: list[str] = None,
             custom_scripts: list[str] = None,
             notify: bool = False):
+        self.job_name = job_name
         self.checks = checks
         self.suites = suites
         self.connectors = connectors
@@ -66,10 +73,12 @@ class Job():
         #TBD: Add support for suites and custom_scripts
         if self.checks is not None and len(self.checks) != 0:
             cmds.append(f'{UNSKRIPT_CTL_BINARY} -rc --check {self.checks[0]} {notify}')
+            print(f'Job: {self.job_name} contains check: {self.checks[0]}')
         if self.connectors is not None and len(self.connectors) != 0:
             # Need to construct the unskript-ctl command like
             # unskript-ctl.sh -rc --types aws,k8s
             connector_types_string = ','.join(self.connectors)
+            print(f'Job: {self.job_name} contains connector types: {connector_types_string}')
             cmds.append(f'{UNSKRIPT_CTL_BINARY} -rc --type {connector_types_string} {notify}')
         self.cmds = cmds
 
@@ -99,6 +108,23 @@ class ConfigParser():
 
         self.parsed_config = retval
 
+    def parse_global(self):
+        """parse_global: This function parses the global section of the config.
+        """
+        print('###################################')
+        print(f'{bcolors.HEADER}Processing global section{bcolors.ENDC}')
+        print('###################################')
+        config = self.parsed_config.get('global')
+        if config is None:
+            print(f"{bcolors.WARNING}Global: Nothing to configure credential with, found empty creds data{bcolors.ENDC}")
+            return
+
+        # Process the audit_period config
+        audit_period = config.get(GLOBAL_CONFIG_AUDIT_PERIOD_KEY_NAME, GLOBAL_DEFAULT_AUDIT_PERIOD)
+        print(f'Global: audit period {audit_period} days')
+        self.audit_period = audit_period
+
+
     def configure_credential(self):
         """configure_credential: This function is used to parse through the creds_dict and
         call the add_creds.sh method to populate the respective credential json
@@ -108,7 +134,7 @@ class ConfigParser():
         print('###################################')
         creds_dict = self.parsed_config.get('credential')
         if creds_dict is None:
-            print(f"ERROR: Nothing to configure credential with, found empty creds data")
+            print(f"{bcolors.WARNING}Credential: Nothing to configure credential with, found empty creds data{bcolors.ENDC}")
             return
 
         for cred_type in creds_dict.keys():
@@ -148,7 +174,7 @@ class ConfigParser():
         print('###################################')
         config = self.parsed_config.get('scheduler')
         if config is None:
-            print(f"No scheduler configuration found")
+            print(f"{bcolors.WARNING}Scheduler: No scheduler configuration found{bcolors.ENDC}")
             return
 
         unskript_crontab_file = "/etc/unskript/unskript_crontab.tab"
@@ -156,7 +182,8 @@ class ConfigParser():
         try:
             for schedule in config:
                 if schedule.get('enable') is False:
-                        continue
+                    print(f'Skipping')
+                    continue
                 cadence = schedule.get('cadence')
                 job_name = schedule.get('job_name')
                 # look up the job name and get the commands
@@ -164,6 +191,7 @@ class ConfigParser():
                 if job is None:
                     print(f'{bcolors.FAIL}Schedule: Unknown job name {job_name}. Please check the jobs section and ensure the job is defined{bcolors.ENDC}')
                     continue
+                print(f'Schedule: cadence {cadence}, job name: {job_name}')
                 script = '; '.join(job.cmds)
                 # TBD: Validate cadence and script is valid
                 crons.append(f'{cadence} {script}')
@@ -172,25 +200,32 @@ class ConfigParser():
             #raise e
             return
 
-        if crons:
-            cmds = []
-            crons_per_line = "\n".join(crons)
-            print(f'Schedule: Programming crontab {crons_per_line}')
-            try:
-                with open(unskript_crontab_file, "w") as f:
-                    # Since crontabs dont inherit the environmnent variables, we have to
-                    # do it explicitly.
-                    for name, value in os.environ.items():
-                        if value != "":
-                            f.write(f'{name}={value}')
-                            f.write("\n")
+        try:
+            with open(unskript_crontab_file, "w") as f:
+                # Since crontabs dont inherit the environmnent variables, we have to
+                # do it explicitly.
+                for name, value in os.environ.items():
+                    if value != "":
+                        f.write(f'{name}={value}')
+                        f.write("\n")
+                if crons:
+                    cmds = []
+                    crons_per_line = "\n".join(crons)
+                    print(f'Schedule: Programming crontab {crons_per_line}')
                     f.write('\n'.join(crons))
                     f.write("\n")
-                cmds = ['crontab', unskript_crontab_file]
-                self.run_command(cmds)
-            except Exception as e:
-                print(f'{bcolors.FAIL}Schedule: Cron programming failed, {e}{bcolors.ENDC}')
-                #raise e
+                # Add the audit period cron job as well, to be run daily.
+                audit_cadence = "0 0 * * *"
+                delete_old_files_command = f'/usr/bin/find {UNSKRIPT_EXECUTION_DIR} -name "*.ipynb" -type f -mtime +{self.audit_period} -exec rm -f {{}} \;'
+                print(f'Adding audit log deletion cron job entry, {audit_cadence} {delete_old_files_command}')
+                f.write(f'{audit_cadence} {delete_old_files_command}')
+                f.write("\n")
+
+            cmds = ['crontab', unskript_crontab_file]
+            self.run_command(cmds)
+        except Exception as e:
+            print(f'{bcolors.FAIL}Schedule: Cron programming failed, {e}{bcolors.ENDC}')
+            #raise e
 
     def parse_jobs(self):
         print('###################################')
@@ -198,19 +233,20 @@ class ConfigParser():
         print('###################################')
         config = self.parsed_config.get('jobs')
         if config is None:
-            print(f'No jobs config found')
+            print(f'{bcolors.WARNING}Jobs: No jobs config found{bcolors.ENDC}')
             return
 
         for job in config:
-            if job.get('enable') is False:
-                continue
             job_name = job.get('name')
             if job_name is None:
-                print(f"{bcolors.OKBLUE}Skipping invalid job, name not found{bcolors.ENDC}")
+                print(f"{bcolors.OKBLUE}Jobs: Skipping invalid job, name not found{bcolors.ENDC}")
+                continue
+            if job.get('enable') is False:
+                print(f'Jobs: Skipping {job_name}')
                 continue
             # Check if the same job name exists
             if job_name in self.jobs:
-                print(f'{bcolors.WARNING}Skipping job name {job_name}, duplicate entry{bcolors.ENDC}')
+                print(f'{bcolors.WARNING}Jobs: Skipping job name {job_name}, duplicate entry{bcolors.ENDC}')
                 continue
             checks = job.get(JOB_CONFIG_CHECKS_KEY_NAME)
             suites = job.get(JOB_CONFIG_SUITES_KEY_NAME)
@@ -221,7 +257,7 @@ class ConfigParser():
             if checks is not None and len(checks) > 1:
                 print(f'{job_name}: NOT SUPPORTED: more than 1 check')
                 continue
-            new_job = Job(checks, suites, connectors, custom_scripts, notify)
+            new_job = Job(job_name, checks, suites, connectors, custom_scripts, notify)
             new_job.parse()
             self.jobs[job_name] = new_job
 
@@ -246,6 +282,7 @@ def main():
     config_parser = ConfigParser(UNSKRIPT_CTL_CONFIG_FILE)
     config_parser.parse_config_yaml()
 
+    config_parser.parse_global()
     config_parser.configure_credential()
     config_parser.parse_jobs()
     config_parser.configure_schedule()
