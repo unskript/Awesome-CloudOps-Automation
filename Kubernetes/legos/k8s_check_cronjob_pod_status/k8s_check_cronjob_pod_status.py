7404c86a248fe768398649474805e378ac7597cf
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-11-13 10:13:58 -0800

    Fixed Check cronjob to use kubectl command

--- Kubernetes/legos/k8s_check_cronjob_pod_status/k8s_check_cronjob_pod_status.py
+++ Kubernetes/legos/k8s_check_cronjob_pod_status/k8s_check_cronjob_pod_status.py
@@ -23,45 +23,47 @@ def k8s_check_cronjob_pod_status_printer(output):
             print(f"CronJob '{issue['cronjob_name']}' Alert: {issue['message']}")
 
 
-def k8s_check_cronjob_pod_status(handle, namespace: str='') -> Tuple:
+def k8s_check_cronjob_pod_status_new(handle, namespace: str = '') -> Tuple:
     """
     Checks the status of the CronJob pods.
 
-    :type handle: object
-    :param handle: The Kubernetes client handle.
+    :type handle: Object
+    :param handle: Object returned from the task.validate(...) function
 
     :type name: str
     :param namespace: Namespace where the CronJob is deployed.
 
     :return: A tuple where the first item has the status if the check and second has a list of failed objects.
     """
-    # Initialize the K8s API clients
-    batch_v1 = client.BatchV1Api(api_client=handle)
-    core_v1 = client.CoreV1Api(api_client=handle)
-
     issues = {"NotAssociated": [], "Pending": [], "UnexpectedState": []}
 
     # Get namespaces to check
     if namespace:
         namespaces = [namespace]
     else:
-        ns_obj = core_v1.list_namespace()
-        namespaces = [ns.metadata.name for ns in ns_obj.items]
+        get_namespace_command = "kubectl get namespaces -o=jsonpath='{.items[*].metadata.name}'"
+        response = handle.run_native_cmd(get_namespace_command)
+
+        if response.returncode != 0:
+            raise Exception(f"Error fetching namespaces: {response.stderr}")
+
+        namespaces = response.stdout.split()
 
     for ns in namespaces:
         # Fetch all CronJobs in the namespace using kubectl
-        get_cronjob_command = f"kubectl get cronjobs -n {ns} -o=jsonpath='{{.items[*].metadata.name}}'"
+        get_cronjob_command = f"kubectl get cronjobs -n {ns} " + "-o=jsonpath={.items[*].metadata.name}"
         response = handle.run_native_cmd(get_cronjob_command)
-
-        if not response or response.stderr:
-            raise Exception(f"Error fetching CronJobs for namespace {ns}: {response.stderr if response else 'empty response'}")
+        
+        if response.returncode != 0:
+            raise Exception(f"Error fetching CronJobs for namespace {ns}: {response.stderr}")
 
         cronjob_names = response.stdout.split()
         for cronjob_name in cronjob_names:
             get_cronjob_details_command = f"kubectl get cronjob {cronjob_name} -n {ns} -o=json"
             try:
                 response = handle.run_native_cmd(get_cronjob_details_command)
-                if response.stderr:
+
+                if response.returncode != 0:
                     raise Exception(f"Error fetching details for CronJob {cronjob_name} in namespace {ns}: {response.stderr}")
             except Exception as e:
                 print(f"Failed to fetch details for CronJob {cronjob_name} in namespace {ns}: {str(e)}")
@@ -77,25 +79,46 @@ def k8s_check_cronjob_pod_status(handle, namespace: str='') -> Tuple:
             time_to_next_run = next_run - now
 
             # Fetch the most recent Job associated with the CronJob
-            jobs = batch_v1.list_namespaced_job(ns)  # Fetch all jobs, and then filter by prefix.
+            get_jobs_command = f"kubectl get jobs -n {ns}" + " -o=json | jq -r '.items[] | select(.metadata.name ) | .metadata.name'"
+            response = handle.run_native_cmd(get_jobs_command)
 
-            associated_jobs = [job for job in jobs.items if job.metadata.name.startswith(cronjob['metadata']['name'])]
-            if not associated_jobs:
-                issues["NotAssociated"].append({"pod_name": cronjob.metadata.name, "namespace": ns})
+            if response.returncode != 0:
+                raise Exception(f"Error fetching jobs for CronJob {cronjob_name} in namespace {ns}: {response.stderr}")
+
+            job_names = response.stdout.split()
+            if not job_names:
+                issues["NotAssociated"].append({"cronjob_name": cronjob_name, "namespace": ns})
                 continue
 
-            latest_job = sorted(associated_jobs, key=lambda x: x.status.start_time, reverse=True)[0]
+            latest_job_name = sorted(job_names, key=None, reverse=True)[0]
+            
 
             # Check job's pods for any issues
-            pods = core_v1.list_namespaced_pod(ns, label_selector=f"job-name={latest_job.metadata.name}")
+            get_pods_command = f"kubectl get pods -n {ns} --selector=job-name={latest_job_name} -o=jsonpath={{.items[*].metadata.name}}"
+            response = handle.run_native_cmd(get_pods_command)
+            if response.returncode != 0:
+                raise Exception(f"Error fetching pods for Job {latest_job_name} in namespace {ns}: {response.stderr}")
 
-            for pod in pods.items:
-                if pod.status.phase == 'Pending' and now - pod.status.start_time > time_to_next_run:
-                    issues["Pending"].append({"pod_name": pod.metadata.name, "namespace": ns})
-                elif pod.status.phase not in ['Running', 'Succeeded']:
-                    issues["UnexpectedState"].append({"pod_name": pod.metadata.name, "namespace": ns, "state": pod.status.phase})
+            pod_names = response.stdout.split()
+            if not pod_names:
+                issues["NotAssociated"].append({"cronjob_name": cronjob_name, "namespace": ns})
+                continue
+
+            for pod_name in pod_names:
+                get_pod_status_command = f"kubectl get pod {pod_name} -n {ns} -o=jsonpath={{.status.phase}}"
+                response = handle.run_native_cmd(get_pod_status_command)
+
+                if response.returncode != 0:
+                    raise Exception(f"Error fetching status for Pod {pod_name} in namespace {ns}: {response.stderr}")
+
+                pod_status = response.stdout.strip()
+
+                if pod_status == 'Pending' and now - datetime.strptime(response.stderr, "%Y-%m-%dT%H:%M:%S%z") > time_to_next_run:
+                    issues["Pending"].append({"pod_name": pod_name, "namespace": ns})
+                elif pod_status not in ['Running', 'Succeeded']:
+                    issues["UnexpectedState"].append({"pod_name": pod_name, "namespace": ns, "state": pod_status})
 
     if all(not val for val in issues.values()):
-        return (True, None)
+        return True, None
     else:
-        return (False, issues)
+        return False, issues
