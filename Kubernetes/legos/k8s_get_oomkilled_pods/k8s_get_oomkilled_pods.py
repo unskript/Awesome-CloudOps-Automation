d77598d8f126e6c26315c1eb69ac86334c7b6bdd
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2023-11-13 16:09:56 -0800

    Convert get_oomkilled_pods to use kubectl

--- Kubernetes/legos/k8s_get_oomkilled_pods/k8s_get_oomkilled_pods.py
+++ Kubernetes/legos/k8s_get_oomkilled_pods/k8s_get_oomkilled_pods.py
@@ -2,11 +2,10 @@
 # Copyright (c) 2023 unSkript.com
 # All rights reserved.
 #
+import json
 import pprint
 from typing import Tuple, Optional
 from pydantic import BaseModel, Field
-from kubernetes import client
-from kubernetes.client.rest import ApiException
 
 
 class InputSchema(BaseModel):
@@ -25,57 +24,47 @@ def k8s_get_oomkilled_pods_printer(output):
     
 
 def k8s_get_oomkilled_pods(handle, namespace: str = "") -> Tuple:
-    """k8s_get_oomkilled_pods This function returns the pods that have OOMKilled event in the container last states
+    """
+    k8s_get_oomkilled_pods This function returns the pods that have OOMKilled event in the container last states
 
-    :type handle: Object
-    :param handle: Object returned from the task.validate(...) function
+    :type handle: object
+    :param handle: Object as returned from the task.validate(...)
 
     :type namespace: str
-    :param namespace: (Optional)String, K8S Namespace as python string
+    :param namespace: (Optional) String, K8S Namespace as python string
 
     :rtype: Status, List of objects of pods, namespaces, and containers that are in OOMKilled state
     """
     result = []
 
-    if handle.client_side_validation is not True:
-        raise ApiException(f"K8S Connector is invalid {handle}")
+    # If namespace is provided, get pods from the specified namespace
+    if namespace:
+        get_pods_command = f"kubectl get pods -n {namespace} -o=json"
+    # If namespace is not provided, get pods from all namespaces
+    else:
+        get_pods_command = "kubectl get pods --all-namespaces -o=json"
 
-    v1 = client.CoreV1Api(api_client=handle)
-
-    # Check whether a namespace is provided, if not fetch all namespaces
     try:
-        if namespace:
-            response = v1.list_namespaced_pod(namespace)
-        else:
-            response = v1.list_pod_for_all_namespaces()
-        
-        if response is None or not hasattr(response, 'items'):
-            raise ApiException("Unexpected response from the Kubernetes API. 'items' not found in the response.")
+        # Execute the kubectl command to get pod information
+        response = handle.run_native_cmd(get_pods_command)
+        pods_info = json.loads(response.stdout)
+    except Exception as e:
+        raise Exception(f"Error fetching pod information: {e.stderr}") from e
 
-        pods = response.items
-
-    except ApiException as e:
-        raise e
-
-    # Check if pods is None or not
-    if pods is None:
-        raise ApiException("No pods returned from the Kubernetes API.")
-
-    for pod in pods:
-        pod_name = pod.metadata.name
-        namespace = pod.metadata.namespace
+    for pod_info in pods_info.get('items', []):
+        pod_name = pod_info['metadata']['name']
+        namespace = pod_info['metadata'].get('namespace', '')
 
         # Ensure container_statuses is not None before iterating
-        container_statuses = pod.status.container_statuses
-        if container_statuses is None:
+        container_statuses = pod_info['status'].get('containerStatuses', [])
+        if not container_statuses:
             continue
 
         # Check each pod for OOMKilled state
         for container_status in container_statuses:
-            container_name = container_status.name
-            last_state = container_status.last_state
-            if last_state and last_state.terminated and last_state.terminated.reason == "OOMKilled":
+            container_name = container_status['name']
+            last_state = container_status.get('lastState', {}).get('terminated', {})
+            if last_state and last_state.get('reason') == "OOMKilled":
                 result.append({"pod": pod_name, "namespace": namespace, "container": container_name})
 
     return (False, result) if result else (True, None)
-
