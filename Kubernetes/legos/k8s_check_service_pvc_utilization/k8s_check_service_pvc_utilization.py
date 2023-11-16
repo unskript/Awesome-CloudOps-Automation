93f15b855ba0250d71ec147e0322ff6718faa0ef
shloka-bhalgat-unskript <110628398+shloka-bhalgat-unskript@users.noreply.github.com>
2023-11-17 04:36:12 +0530

    Fix: K8s PVC utilization check (#935)

--- Kubernetes/legos/k8s_check_service_pvc_utilization/k8s_check_service_pvc_utilization.py
+++ Kubernetes/legos/k8s_check_service_pvc_utilization/k8s_check_service_pvc_utilization.py
@@ -6,6 +6,7 @@ import re
 import json
 from typing import Tuple, Optional
 from pydantic import BaseModel, Field
+from kubernetes.client.rest import ApiException
 
 
 class InputSchema(BaseModel):
@@ -63,7 +64,7 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
         get_service_namespace_command = f"kubectl get service {service_name} -o=jsonpath='{{.metadata.namespace}}'"
         response = handle.run_native_cmd(get_service_namespace_command)
         if not response or response.stderr:
-            raise Exception(f"Error fetching namespace for service {service_name}: {response.stderr if response else 'empty response'}")
+            raise ApiException(f"Error fetching namespace for service {service_name}: {response.stderr if response else 'empty response'}")
         namespace = response.stdout.strip()
         print(f"Service {service_name} belongs to namespace: {namespace}")
 
@@ -72,7 +73,7 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
         get_ns_command = "kubectl config view --minify --output 'jsonpath={..namespace}'"
         response = handle.run_native_cmd(get_ns_command)
         if not response or response.stderr:
-            raise Exception(f"Error fetching current namespace: {response.stderr if response else 'empty response'}")
+            raise ApiException(f"Error fetching current namespace: {response.stderr if response else 'empty response'}")
         namespace = response.stdout.strip() or "default"
         print(f"Operating in the current namespace: {namespace}")
 
@@ -82,10 +83,9 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
         get_all_services_command = f"kubectl get svc -n {namespace} -o=jsonpath='{{.items[*].metadata.name}}'"
         response = handle.run_native_cmd(get_all_services_command)
         if not response or response.stderr:
-            raise Exception(f"Error fetching services in namespace {namespace}: {response.stderr if response else 'empty response'}")
+            raise ApiException(f"Error fetching services in namespace {namespace}: {response.stderr if response else 'empty response'}")
         services_to_check = response.stdout.strip().split()
 
-    utility_pod_created = False
     alert_pvcs_all_services = []
     services_without_pvcs = []
 
@@ -104,85 +104,77 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
         get_pod_command = f"kubectl get pods -n {namespace} -l {label_selector} -o=jsonpath='{{.items[0].metadata.name}}'"
         response = handle.run_native_cmd(get_pod_command)
         if not response or response.stderr:
-            raise Exception(f"Error while executing command ({get_pod_command}): {response.stderr if response else 'empty response'}")
+            raise ApiException(f"Error while executing command ({get_pod_command}): {response.stderr if response else 'empty response'}")
         pod_name = response.stdout.strip()
 
         # Fetch PVCs attached to the pod
         get_pvc_names_command = f"kubectl get pod {pod_name} -n {namespace} -o=jsonpath='{{.spec.volumes[*].persistentVolumeClaim.claimName}}'"
         response = handle.run_native_cmd(get_pvc_names_command)
         if not response or response.stderr:
-            raise Exception(f"Error while executing command ({get_pvc_names_command}): {response.stderr if response else 'empty response'}")
+            raise ApiException(f"Error while executing command ({get_pvc_names_command}): {response.stderr if response else 'empty response'}")
         pvc_names = response.stdout.strip().split()
 
         # If there are no PVCs for this service, continue to the next one
         if not pvc_names:
             services_without_pvcs.append(svc)
             continue
-        if not utility_pod_created:
-            utility_pod_created = True 
-            print(f"Creating utility pod to check {len(pvc_names)} PVC(s) for service {service_name}")
 
-            utility_pod_spec = {
-                "apiVersion": "v1",
-                "kind": "Pod",
-                "metadata": {
-                    "name": "pvc-utility-pod",
-                    "namespace": namespace
-                },
-                "spec": {
-                    "containers": [
-                        {
-                            "name": "util-container",
-                            "image": "busybox:latest",
-                            "command": ["/bin/sh", "-c", "sleep 3600"],
-                            "volumeMounts": [{"name": f"volume-{idx}", "mountPath": f"/data-{idx}"} for idx, _ in enumerate(pvc_names)]
-                        }
-                    ],
-                    "volumes": [{"name": f"volume-{idx}", "persistentVolumeClaim": {"claimName": name}} for idx, name in enumerate(pvc_names)]
-                }
-            }
+        # Fetch the Pod JSON
+        get_pod_json_command = f"kubectl get pod {pod_name} -n {namespace} -o json"
+        pod_json_output = handle.run_native_cmd(get_pod_json_command)
+        if not pod_json_output or pod_json_output.stderr:
+            raise ApiException(f"Error fetching pod json for {pod_name}: {pod_json_output.stderr if pod_json_output else 'empty response'}")
+        pod_data = json.loads(pod_json_output.stdout)
 
-            with open("/tmp/utility-pod.yaml", "w") as f:
-                json.dump(utility_pod_spec, f)
-            apply_command = "kubectl apply -f /tmp/utility-pod.yaml"
-            response = handle.run_native_cmd(apply_command)
+        # Process the Pod JSON to find PVC mounts
+        pvc_mounts = []
+        for container in pod_data['spec']['containers']:
+            container_name = container['name']
+            for mount in container.get('volumeMounts', []):
+                mount_path = mount['mountPath']
+                volume_name = mount['name']
+                # Match the volume name with PVCs in the Pod spec
+                for volume in pod_data['spec']['volumes']:
+                    if 'persistentVolumeClaim' in volume and volume['name'] == volume_name:
+                        pvc_name = volume['persistentVolumeClaim']['claimName']
+                        pvc_mounts.append({"container_name": container_name, "mount_path": mount_path, "pvc_name": pvc_name})
+        alert_pvcs = []
+        # Iterate over pvc_mounts to calculate used space
+        for mount in pvc_mounts:
+            container_name = mount['container_name']
+            mount_path = mount['mount_path']
+            pvc_name = mount['pvc_name']
+
+            # Execute the du command
+            du_command = f"kubectl exec -n {namespace} {pod_name} -c {container_name} -- du -sh {mount_path}"
+            du_output = handle.run_native_cmd(du_command)
+            if not du_output or du_output.stderr:
+                print(f"Error while calculating used space for mount path {mount_path} in container {container_name}: {du_output.stderr if du_output else 'empty response'}")
+                continue
+
+            used_space = du_output.stdout.split()[0]
+            if used_space[-1] == "G":
+                used_space_gib = float(used_space[:-1])
+            elif used_space[-1] == "M":
+                used_space_gib = float(used_space[:-1]) / 1024
+            elif used_space[-1] == "K":
+                used_space_gib = float(used_space[:-1]) / (1024 * 1024)
+            else:
+                raise ValueError("Unexpected unit in du output")
+
+            get_pvc_capacity_command = f"kubectl get pvc {pvc_name} -n {namespace} -o=jsonpath='{{.status.capacity.storage}}'"
+            response = handle.run_native_cmd(get_pvc_capacity_command)
             if not response or response.stderr:
-                raise Exception(f"Error while applying utility pod spec: {response.stderr if response else 'empty response'}")
-            wait_command = "kubectl wait --for=condition=Ready pod/pvc-utility-pod --timeout=60s -n " + namespace
-            response = handle.run_native_cmd(wait_command)
-            if not response or response.stderr:
-                raise Exception(f"Utility pod did not become ready: {response.stderr if response else 'empty response'}")
+                raise ApiException(f"Error while executing command ({get_pvc_capacity_command}): {response.stderr if response else 'empty response'}")
 
-            alert_pvcs = []
-            for idx, pvc_name in enumerate(pvc_names):
-                du_command = f"kubectl exec -n {namespace} pvc-utility-pod -- du -sh /data-{idx}"
-                du_output = handle.run_native_cmd(du_command)
-                if not du_output or du_output.stderr:
-                    raise Exception(f"Error while calculating used space for {pvc_name}: {du_output.stderr if du_output else 'empty response'}")
-                used_space = du_output.stdout.split()[0]
-                if used_space[-1] == "G":
-                    used_space_gib = float(used_space[:-1])
-                elif used_space[-1] == "M":
-                    used_space_gib = float(used_space[:-1]) / 1024
-                elif used_space[-1] == "K":
-                    used_space_gib = float(used_space[:-1]) / (1024 * 1024)
-                else:
-                    raise ValueError("Unexpected unit in du output")
-                get_pvc_capacity_command = f"kubectl get pvc {pvc_name} -n {namespace} -o=jsonpath='{{.status.capacity.storage}}'"
-                response = handle.run_native_cmd(get_pvc_capacity_command)
-                if not response or response.stderr:
-                    raise Exception(f"Error while executing command ({get_pvc_capacity_command}): {response.stderr if response else 'empty response'}")
-                total_capacity_str = response.stdout.strip()
-                total_capacity_gib = float(re.findall(r"(\d+)", total_capacity_str)[0])
-                used_percentage = (used_space_gib / total_capacity_gib) * 100
-                if used_percentage > threshold:
-                    alert_pvcs.append({"pvc_name": pvc_name, "used": used_space, "capacity": total_capacity_str})
-            alert_pvcs_all_services.extend(alert_pvcs)
-    if utility_pod_created:
-        print("Deleting utility pod...")
-        delete_command = "kubectl delete -f /tmp/utility-pod.yaml"
-        handle.run_native_cmd(delete_command)
-    # Print out services without PVCs
+            total_capacity_str = response.stdout.strip()
+            total_capacity_gib = float(re.findall(r"(\d+)", total_capacity_str)[0])
+            used_percentage = (used_space_gib / total_capacity_gib) * 100
+
+            if used_percentage > threshold:
+                alert_pvcs.append({"pvc_name": pvc_name, "mount_path": mount_path, "used": used_space, "capacity": total_capacity_str})
+
+        alert_pvcs_all_services.extend(alert_pvcs)
     if services_without_pvcs:
         print("Following services do not have any PVCs attached:")
         for service in services_without_pvcs:
