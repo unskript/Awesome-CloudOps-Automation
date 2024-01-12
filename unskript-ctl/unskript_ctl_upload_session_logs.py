844999b8a51e1c30715bfaed7133533607544125
sagar-salvi-unskript <sagar.salvi@unskript.com>
2024-01-12 12:54:04 +0530

    Fixed PR comments

--- unskript-ctl/unskript_ctl_upload_session_logs.py
+++ unskript-ctl/unskript_ctl_upload_session_logs.py
@@ -17,14 +17,23 @@ import requests
 import subprocess
 
 
-source_directory = '/var/unskript/sessions/logs'
-destination_directory = '/var/unskript/sessions/uploads'
-tar_file_path = '/var/unskript/sessions/session_logs.tgz'
-command = f'find {destination_directory} -type f -exec stat -c "%W %n" {{}} \\;'
+SOURCE_DIRECTORY = '/var/unskript/sessions/logs'
+DESTINATION_DIRECTORY = '/var/unskript/sessions/uploads'
+TAR_FILE_PATH = '/var/unskript/sessions/session_logs.tgz'
+RTS_HOST = 'http://10.8.0.1:6443'
+URL_PATH = '/v1alpha1/sessions/logs'
+LOG_FILE_PATH = '/var/log/unskript/upload_script.log'
+command = f'find {DESTINATION_DIRECTORY} -type f -exec stat -c "%W %n" {{}} \\;'
 
-logging.basicConfig(filename="/var/log/unskript/upload_script.log",
-                    level=logging.DEBUG,
-                    format=f'%(asctime)s - %(levelname)s - %(message)s')
+# Set logging config
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 
 def get_logs_timestamps():
     # Run the command and capture the output
@@ -38,59 +47,59 @@ def get_logs_timestamps():
             filename = full_path.split('/')[-1]  # Extracting the filename from the full path
             file_timestamp_dict[filename.split('.log')[0]] = int(timestamp)
 
-    # Print the resulting dictionary
-    logging.info(file_timestamp_dict)
     return file_timestamp_dict
 
 def upload_session_logs():
     # Capture start time
     start_time = datetime.now()
-    logging.info(f'Start Time: {start_time}')
-    if not os.path.exists(destination_directory):
-        os.makedirs(destination_directory)
+    logger.info(f'Start Time: {start_time}')
+    if not os.path.exists(DESTINATION_DIRECTORY):
+        os.makedirs(DESTINATION_DIRECTORY)
     # Move files from logs to uploads
-    for filename in os.listdir(source_directory):
-        source_path = os.path.join(source_directory, filename)
-        destination_path = os.path.join(destination_directory, filename)
-        shutil.move(source_path, destination_path)
+    for filename in os.listdir(SOURCE_DIRECTORY):
+        source_path = os.path.join(SOURCE_DIRECTORY, filename)
+        destination_path = os.path.join(DESTINATION_DIRECTORY, filename)
+        try:
+            shutil.move(source_path, destination_path)
+        except Exception as e:
+            logger.error("File move error: %s", str(e))
+            return
     # Create a tar.gz archive
-    with tarfile.open(tar_file_path, 'w:gz') as tar:
-        tar.add(destination_directory, arcname='uploads')
-    
-    files = [f for f in os.listdir(destination_directory) if os.path.isfile(os.path.join(destination_directory, f))]
-    logging.info(f'Num files uploaded: {len(files)}')
+    with tarfile.open(TAR_FILE_PATH, 'w:gz') as tar:
+        tar.add(DESTINATION_DIRECTORY, arcname='uploads')
     # Upload to rts
     try:
         upload_logs_files()
     except Exception as e:
-        logging.error(f'Error uploading files: {str(e)}')
+        logger.error(str(e))
     
-    # if upload fails don't remove files in upload directory else remove it
-    # remove tar.gz file in tar_file_path
-    os.remove(tar_file_path)
-    # Remove the files from the uploads folder
-    shutil.rmtree(destination_directory)
     # Capture end time
     end_time = datetime.now()
-    logging.info(f'End Time: {end_time}')
+    logger.info(f'End Time: {end_time}')
 
 def upload_logs_files():
-    url = 'http://10.8.0.1:6443/v1alpha1/sessions/logs'
-
     # Open the file in binary mode
-    with open(tar_file_path, 'rb') as file:
-        # Set up the files parameter with a tuple containing the filename and file object
-        files = {'file': (tar_file_path, file)}
-
-        # Make the POST request with the files parameter
-        response = requests.post(url, files=files)
-
+    try:
+        with open(TAR_FILE_PATH, 'rb') as file:
+            # Set up the files parameter with a tuple containing the filename and file object
+            files = {'file': (TAR_FILE_PATH, file)}
+            url = f'{RTS_HOST}{URL_PATH}'
+            # Make the POST request with the files parameter
+            try:
+                response = requests.post(url, files=files)
+            except Exception:
+                logger.error("Status Code: %s. Response: %s", response.status_code, response.text)
+    except FileNotFoundError:
+        logger.error("File not found. Tar file path: %s",TAR_FILE_PATH)
+        return
     # Check the response
     if response.status_code == 204:
-        logging.info("File uploaded successfully.")
-    else:
-        logging.info(f"Failed to upload file. Status code: {response.status_code}")
-        logging.info(response.text)
+        num_files = [f for f in os.listdir(DESTINATION_DIRECTORY) if os.path.isfile(os.path.join(DESTINATION_DIRECTORY, f))]
+        logger.info("%d file(s) uploaded successfully", len(num_files))
+        # Remove the files from the uploads folder
+        shutil.rmtree(DESTINATION_DIRECTORY)
+    # Remove Tar file
+    os.remove(TAR_FILE_PATH)
     
 if __name__ == "__main__":
     upload_session_logs()
