01c68bff72363487b05f9dd60cf90c08d5012193
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2024-02-01 09:30:36 -0800

    Implementing REST wrapper for keycloak legos

--- Keycloak/legos/keycloak_get_audit_report/keycloak_get_audit_report.py
+++ Keycloak/legos/keycloak_get_audit_report/keycloak_get_audit_report.py
@@ -1,10 +1,13 @@
 #
-# Copyright (c) 2023 unSkript.com
+# Copyright (c) 2024 unSkript.com
 # All rights reserved.
 #
+import requests
+
 from typing import List
 from tabulate import tabulate
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class InputSchema(BaseModel):
@@ -17,20 +20,27 @@ def keycloak_get_audit_report_printer(output):
         return
 
     # Extract relevant event data for tabulation
-    table_data = []
+    table_data = [["Time", "Type", "User ID", "Client ID", "IP Address", "Error"]]
     for event in output:
-        table_data.append({
-            "Time": event['time'],
-            "Type": event['type'],
-            "User ID": event['userId'],
-            "Client ID": event['clientId'],
-            "IP Address": event['ipAddress'],
-            "Error": event.get('error', '')
-        })
+        time = event.get('time') if event.get('time') else ''
+        _type = event.get('type') if event.get('type') else \
+               event.get('operationType') if event.get('operationType') else ''
+        user_id = event.get('userId') if event.get('userId') else \
+                  event.get('authDetails').get('userId', '') if event.get('authDetails') else ''
+        client_id = event.get('clientId') if event.get('clientId') else \
+                  event.get('authDetails').get('clientId', '') if event.get('authDetails') else ''
+        ip_addr = event.get('ipAddress') if event.get('ipAddress') else \
+                  event.get('authDetails').get('ipAddress', '') if event.get('authDetails') else ''
+        error = event.get('error', '')
+        
+        table_data.append([datetime.fromtimestamp(time/1000).strftime('%Y-%m-%d %H:%M:%S'), 
+                           _type, 
+                           user_id, 
+                           client_id, 
+                           ip_addr, 
+                           error])
 
-    # Convert list of dictionaries to tabulated format
-    headers = ["Time", "Type", "User ID", "Client ID", "IP Address", "Error"]
-    print(tabulate(table_data, headers=headers, tablefmt="grid"))
+    print(tabulate(table_data, headers='firstrow', tablefmt="grid"))
 
 
 def keycloak_get_audit_report(handle) -> List:
@@ -48,6 +58,25 @@ def keycloak_get_audit_report(handle) -> List:
         return events if events else []
 
     except Exception as e:
-        raise e
+        try:
+            # Exception could occur if keycloak package was not found
+            # in such case try if we can import UnskriptKeycloakWrapper
+            from unskript.connectors.keycloak import UnskriptKeycloakWrapper
+            from unskript.legos.utils import get_keycloak_token
+        except:
+            raise e
+        
+        if not isinstance(handle, UnskriptKeycloakWrapper):
+            raise ValueError(f"Unable to Find Keycloak Package! {e}")
+        access_token = get_keycloak_token(handle)
+        events_url = f"{handle.server_url}/admin/realms/{handle.realm_name}/events"
+        headers = {"Authorization": f"Bearer {access_token}"}
 
+        response = requests.get(events_url, headers=headers)
+        response.raise_for_status()
+
+        events = response.json()
+
+        return events if events else []
+        
 
--- Keycloak/legos/keycloak_get_service_health/keycloak_get_service_health.py
+++ Keycloak/legos/keycloak_get_service_health/keycloak_get_service_health.py
@@ -1,7 +1,9 @@
 #
-# Copyright (c) 2023 unSkript.com
+# Copyright (c) 2024 unSkript.com
 # All rights reserved.
 #
+
+import requests
 
 from typing import Tuple
 from pydantic import BaseModel
@@ -29,7 +31,34 @@ def keycloak_get_service_health(handle) -> Tuple:
         return (True, None)
 
     except Exception as e:
-        raise e
+        try:
+            # Exception could occur if keycloak package was not found
+            # in such case try if we can import UnskriptKeycloakWrapper
+            from unskript.connectors.keycloak import UnskriptKeycloakWrapper
+            from unskript.legos.utils import get_keycloak_token
+        except:
+            raise e
+        
+        if not isinstance(handle, UnskriptKeycloakWrapper):
+            raise ValueError(f"Unable to Find Keycloak Package! {e}")
+        
+        access_token = get_keycloak_token(handle)
+        realms_url = f"{handle.server_url}/admin/realms"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        response = requests.get(realms_url, headers=headers)
+        response.raise_for_status()
+
+        available_realms = response.json()
+        result = False
+        if handle.realm_name and available_realms:
+            result = any(realm.get("realm") == handle.realm_name for realm in available_realms)
+
+        if not result:
+            return (False, available_realms)
+        
+        return (True, None)
+
+
 
 def keycloak_get_service_health_printer(output):
     is_healthy, realms = output
