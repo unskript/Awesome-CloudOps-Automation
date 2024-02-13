712ca4608bef69d99c13237274de3b6857b92b30
Jayasimha Raghavan <87547684+jayasimha-raghavan-unskript@users.noreply.github.com>
2024-02-12 18:59:52 -0800

    rewrote the logic to reduce CPU utilization (#984)
    Co-authored-by: Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>

--- Kubernetes/legos/k8s_check_service_pvc_utilization/k8s_check_service_pvc_utilization.py
+++ Kubernetes/legos/k8s_check_service_pvc_utilization/k8s_check_service_pvc_utilization.py
@@ -8,19 +8,18 @@ from typing import Tuple, Optional
 from pydantic import BaseModel, Field
 from kubernetes.client.rest import ApiException
 
-
 class InputSchema(BaseModel):
-    namespace: Optional[str] = Field(..., description='The namespace in which the service resides.', title='Namespace')
-    service_name: Optional[str] = Field(
-        ...,
-        description='The name of the service for which the used PVC size needs to be checked.',
-        title='K8s Sservice name',
-    )
-    threshold: Optional[int] = Field(
-        80,
-        description='Percentage threshold for utilized PVC disk size.E.g., a 80% threshold checks if the utilized space exceeds 80% of the total PVC capacity.',
-        title='Threshold (in %)',
-    )
+     namespace: Optional[str] = Field(..., description='The namespace in which the service resides.', title='Namespace')
+     service_name: Optional[str] = Field(
+         ...,
+         description='The name of the service for which the used PVC size needs to be checked.',
+         title='K8s Sservice name',
+     )
+     threshold: Optional[int] = Field(
+         80,
+         description='Percentage threshold for utilized PVC disk size.E.g., a 80% threshold checks if the utilized space exceeds 80% of the total PVC capacity.',
+         title='Threshold (in %)',
+     )
 
 
 def k8s_check_service_pvc_utilization_printer(output):
@@ -34,7 +33,6 @@ def k8s_check_service_pvc_utilization_printer(output):
         for pvc in pvc_info:
             print(f"PVC: {pvc['pvc_name']} - Utilized: {pvc['used']} of {pvc['capacity']}")
         print("-" * 40)
-
 
 def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace: str = "", threshold: int = 80) -> Tuple:
     """
@@ -52,7 +50,7 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
 
     :type threshold: int
     :param threshold: Percentage threshold for utilized PVC disk size.
-                      E.g., a 80% threshold checks if the utilized space exceeds 80% of the total PVC capacity.
+                        E.g., a 80% threshold checks if the utilized space exceeds 80% of the total PVC capacity.
 
     :type namespace: str
     :param namespace: The namespace in which the service resides.
@@ -90,36 +88,33 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
     services_without_pvcs = []
 
     for svc in services_to_check:
-
-        # Get service's pod based on its labels
-        get_service_labels_command = f"kubectl get service {svc} -n {namespace} -o=jsonpath='{{.spec.selector}}'"
+        # Get label associated with the service
+        get_service_labels_command = f"kubectl get services {svc} -n {namespace} -o=jsonpath='{{.spec.selector}}'"
         response = handle.run_native_cmd(get_service_labels_command)
         if not response.stdout.strip():
-            # No labels found for a particular service. Skipping... 
+            # No labels found for a particular service. Skipping...
             continue
         labels_dict = json.loads(response.stdout.replace("'", "\""))
         label_selector = ",".join([f"{k}={v}" for k, v in labels_dict.items()])
 
         # Fetch the pod attached to this service
-        get_pod_command = f"kubectl get pods -n {namespace} -l {label_selector} -o json"
+        get_pod_command = f"kubectl get pods -n {namespace} -l {label_selector} -o=jsonpath='{{.items[0].metadata.name}}'"
         response = handle.run_native_cmd(get_pod_command)
         if not response or response.stderr:
             raise ApiException(f"Error while executing command ({get_pod_command}): {response.stderr if response else 'empty response'}")
 
-        pod_data = json.loads(response.stdout)
-        if not pod_data.get("items"):
+        pod_name = response.stdout.strip()
+        if not pod_name:
             print(f"No pods found for service {svc} in namespace {namespace} with labels {label_selector}")
             continue
-
-        pod_name = pod_data["items"][0]["metadata"]["name"]
-
+        
         # Fetch PVCs attached to the pod
         get_pvc_names_command = f"kubectl get pod {pod_name} -n {namespace} -o=jsonpath='{{.spec.volumes[*].persistentVolumeClaim.claimName}}'"
         response = handle.run_native_cmd(get_pvc_names_command)
         if not response or response.stderr:
             raise ApiException(f"Error while executing command ({get_pvc_names_command}): {response.stderr if response else 'empty response'}")
         pvc_names = response.stdout.strip().split()
-
+                
         # If there are no PVCs for this service, continue to the next one
         if not pvc_names:
             services_without_pvcs.append(svc)
@@ -132,53 +127,41 @@ def k8s_check_service_pvc_utilization(handle, service_name: str = "", namespace:
             raise ApiException(f"Error fetching pod json for {pod_name}: {pod_json_output.stderr if pod_json_output else 'empty response'}")
         pod_data = json.loads(pod_json_output.stdout)
 
-        # Process the Pod JSON to find PVC mounts
-        pvc_mounts = []
-        for container in pod_data['spec']['containers']:
-            container_name = container['name']
-            for mount in container.get('volumeMounts', []):
-                mount_path = mount['mountPath']
-                volume_name = mount['name']
-                # Match the volume name with PVCs in the Pod spec
-                for volume in pod_data['spec']['volumes']:
-                    if 'persistentVolumeClaim' in volume and volume['name'] == volume_name:
-                        pvc_name = volume['persistentVolumeClaim']['claimName']
-                        pvc_mounts.append({"container_name": container_name, "mount_path": mount_path, "pvc_name": pvc_name})
+        pvc_mounts = [
+            {"container_name": container['name'],
+            "mount_path": mount['mountPath'],
+            "pvc_name": volume['persistentVolumeClaim']['claimName']}
+            for container in pod_data['spec']['containers']
+            for mount in container.get('volumeMounts', [])
+            for volume in pod_data['spec']['volumes']
+            if 'persistentVolumeClaim' in volume and volume['name'] == mount['name']
+        ]
+
         alert_pvcs = []
-        # Iterate over pvc_mounts to calculate used space
+        all_pvcs = []
+
         for mount in pvc_mounts:
             container_name = mount['container_name']
             mount_path = mount['mount_path']
             pvc_name = mount['pvc_name']
+            all_pvcs.append({"pvc_name": pvc_name, "mount_path": mount_path, "used": None, "capacity": None})
 
-            # Execute the du command
-            du_command = f"kubectl exec -n {namespace} {pod_name} -c {container_name} -- du -sh {mount_path}"
-            du_output = handle.run_native_cmd(du_command)
-            if not du_output or du_output.stderr:
-                print(f"Error while calculating used space for mount path {mount_path} in container {container_name}: {du_output.stderr if du_output else 'empty response'}")
-                continue
+        all_mounts = [mount.get('mount_path') for mount in pvc_mounts]
+        all_mounts = " ".join(all_mounts).strip()
+        du_command = f"kubectl exec -n {namespace} {pod_name} -c {container_name} -- df -kh {all_mounts} | grep -v Filesystem"
+        du_output = handle.run_native_cmd(du_command)
 
-            used_space = du_output.stdout.split()[0]
-            if used_space[-1] == "G":
-                used_space_gib = float(used_space[:-1])
-            elif used_space[-1] == "M":
-                used_space_gib = float(used_space[:-1]) / 1024
-            elif used_space[-1] == "K":
-                used_space_gib = float(used_space[:-1]) / (1024 * 1024)
-            else:
-                raise ValueError("Unexpected unit in du output")
 
-            get_pvc_capacity_command = f"kubectl get pvc {pvc_name} -n {namespace} -o=jsonpath='{{.status.capacity.storage}}'"
-            response = handle.run_native_cmd(get_pvc_capacity_command)
-            if not response or response.stderr:
-                raise ApiException(f"Error while executing command ({get_pvc_capacity_command}): {response.stderr if response else 'empty response'}")
-
-            total_capacity_str = response.stdout.strip()
-            total_capacity_gib = float(re.findall(r"(\d+)", total_capacity_str)[0])
-            used_percentage = (used_space_gib / total_capacity_gib) * 100
-
-            if used_percentage > threshold:
-                alert_pvcs.append({"pvc_name": pvc_name, "mount_path": mount_path, "used": used_space, "capacity": total_capacity_str})
+        if du_output and not du_output.stderr:
+            used_space = du_output.stdout.strip()
+            for idx, space in enumerate([used_space]):
+                space = space.split()
+                used_percentage = int(space[-2].replace('%', ''))
+                total_capacity_str = space[1].replace('%', '')
+                all_pvcs[idx]["used"] = used_percentage
+                all_pvcs[idx]["capacity"] =  total_capacity_str
+                if used_percentage > threshold:
+                    alert_pvcs.append(all_pvcs[idx])
 
         alert_pvcs_all_services.extend(alert_pvcs)
     if services_without_pvcs:
