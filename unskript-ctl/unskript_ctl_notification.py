af3c4fc408432f18437727902ca3d67fede976ee
Jayasimha Raghavan <jayasimharaghavan@Jayasimhas-MacBook-Pro.local>
2024-02-19 16:52:51 -0800

    PR Comments

--- unskript-ctl/unskript_ctl_notification.py
+++ unskript-ctl/unskript_ctl_notification.py
@@ -379,7 +379,7 @@ class EmailNotification(NotificationFactory):
             <center>
             <h1> {email_title} </h1>
             <h3> <strong>Tested On <br> {datetime.now().strftime("%a %b %d %I:%M:%S %p %Y %Z")} </strong></h3>
-            <h4> <strong>Build #: <br> {BUILD_NUMBER} </strong></h4>
+            <h4> <strong>Version : {BUILD_NUMBER} </strong></h4><br>
             </center>
             '''
         return message
