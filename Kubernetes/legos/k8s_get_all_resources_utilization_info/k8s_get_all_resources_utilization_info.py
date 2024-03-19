a5c5a7acc83d3d05fbd95aa22a60c69d11f57c51
shloka-bhalgat-unskript <shloka@unskript.com>
2024-03-19 19:50:14 +0530

    Add failed pods to filter

--- Kubernetes/legos/k8s_get_all_resources_utilization_info/k8s_get_all_resources_utilization_info.py
+++ Kubernetes/legos/k8s_get_all_resources_utilization_info/k8s_get_all_resources_utilization_info.py
@@ -90,6 +90,9 @@ def k8s_get_all_resources_utilization_info(handle, namespace: str = "") -> Dict:
 
             if resource == 'pods':
                 status = item['status']['phase']
+                 # Skip pods in Succeeded or Completed state as they dont have any utilization
+                if status in ['Succeeded', 'Completed', 'Failed']:
+                    continue
                 key = (ns, name)
                 cpu_usage, memory_usage = utilization_map.get(key, ('N/A', 'N/A'))
                 data[resource].append([ns, name, status, cpu_usage, memory_usage])
