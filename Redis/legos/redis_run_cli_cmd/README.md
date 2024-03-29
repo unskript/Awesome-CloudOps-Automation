67d7ed97ef482b152bfc040742c6bf8668da6858
shloka-bhalgat-unskript <shloka@unskript.com>
2024-03-29 17:36:00 +0530

    Redis run CLI command action

---
+++ Redis/legos/redis_run_cli_cmd/README.md
@@ -0,0 +1,25 @@
+[<img align="left" src="https://unskript.com/assets/favicon.png" width="100" height="100" style="padding-right: 5px">](https://unskript.com/assets/favicon.png) 
+<h1>Redis run Cli command</h1>
+
+## Description
+This action runs a redis-cli command
+
+
+## Lego Details
+
+    redis_run_cli_cmd(handle, commands:list)
+
+        handle: Object of type unSkript Redis Connector
+        commands: List of mongosh commands 
+
+## Lego Input
+This Lego take two input handle, commands.
+
+## Lego Output
+Here is a sample output.
+<img src="./1.png">
+
+
+## See it in Action
+
+You can see this Lego in action following this link [unSkript Live](https://us.app.unskript.io)
--- Redis/legos/redis_run_cli_cmd/redis_run_cli_cmd.py
+++ Redis/legos/redis_run_cli_cmd/redis_run_cli_cmd.py
@@ -10,51 +10,46 @@ import subprocess
 
 class InputSchema(BaseModel):
     commands: list = Field(
-        title='List of mongosh commands',
-        description='List of mongosh commands'
+        title='List of redis-cli commands',
+        description='List of redis-cli commands. Example- ["info","--bigkeys"]'
     )
 
 
-def mongodb_run_cli_cmd_printer(output):
+def redis_run_cli_cmd_printer(output):
     if not output:
         print("No output generated")
         return
     
     for result_dict in output:
         for command, cmd_output in result_dict.items():
-            print(f"Mongosh Command: {command}\nOutput: {cmd_output}\n")
+            print(f"Redis Command: {command}\nOutput: {cmd_output}\n")
 
 
-def mongodb_run_cli_cmd(handle, commands:list) -> List:
+def redis_run_cli_cmd(handle, commands:list) -> List:
     """
-    mongodb_run_cli_cmd runs mongocli command with command as the parameter
+    redis_run_cli_cmd runs redis-cli command with command as the parameter
 
     :type handle: object
     :param handle: Object returned from task.validate(...).
 
     :type commands: list
-    :param commands: List of mongosh commands 
+    :param commands: List of redis commands 
 
     :rtype: String output from the commands
     
     """
-    MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
-    MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
-    MONGODB_HOSTNAME = os.getenv('MONGODB_HOSTNAME', 'localhost')
-    MONGODB_PORT = int(os.getenv('MONGODB_PORT', 27017))
+    REDIS_HOSTNAME = os.getenv('REDIS_HOSTNAME', 'localhost')
+    REDIS_PORT = os.getenv('REDIS_PORT', '6379')
 
-    # Format the connection string for mongosh
-    connection_string = f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_HOSTNAME}:{MONGODB_PORT}"
     command_outputs = []
 
     for command in commands:
         cmd = [
-            "mongosh",
-            connection_string,
-            "--quiet",
-            "--eval",
-            command
-        ]
+        "redis-cli",
+        "-h", REDIS_HOSTNAME,
+        "-p", REDIS_PORT,
+        command
+    ]
         try:
             result = subprocess.run(cmd, capture_output=True, text=True)
             if result.stderr:
@@ -64,5 +59,4 @@ def mongodb_run_cli_cmd(handle, commands:list) -> List:
                 command_outputs.append({command: output})
         except Exception as e:
             command_outputs.append({command: f"Exception: {str(e)}"})
-
     return command_outputs
