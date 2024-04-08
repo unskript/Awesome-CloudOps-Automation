cdc6b839d74d02c836ed7d9ae8acb17ae370f528
Jayasimha Raghavan <jayasimha@unskript.com>
2024-04-08 13:04:15 -0700

    Adding logic to check oom killed and restart based on time

--- Kubernetes/legos/k8s_get_pods_with_high_restart/k8s_get_pods_with_high_restart.py
+++ Kubernetes/legos/k8s_get_pods_with_high_restart/k8s_get_pods_with_high_restart.py
@@ -2,11 +2,15 @@
 # Copyright (c) 2023 unSkript.com
 # All rights reserved.
 #
+import datetime 
+from datetime import timezone
 from typing import Tuple
 from pydantic import BaseModel, Field
 from kubernetes import client
 from kubernetes.client.rest import ApiException
 
+# Constants used in this file
+INTERVAL_TO_CHECK = 24  # In hours
 
 class InputSchema(BaseModel):
     namespace: str = Field(
@@ -54,10 +58,28 @@ def k8s_get_pods_with_high_restart(handle, namespace: str = '', threshold: int =
         raise Exception(f"Error occurred while accessing Kubernetes API: {e}")
 
     retval = []
+    
+    # It is not enough to check if the restart count is more than the threshold 
+    # we should check if the last time the pod got restarted is not within the 24 hours.
+    # If it is, then we need to flag it. If not, it could be that the pod restarted at 
+    # some time, but have been stable since then. 
+
+    # Lets take current time and reference time that is 24 hours ago.
+    current_time = datetime.datetime.now(timezone.utc)
+    interval_time_to_check = current_time - datetime.timedelta(hours=INTERVAL_TO_CHECK)
+    interval_time_to_check = interval_time_to_check.replace(tzinfo=timezone.utc)
+
     for pod in pods:
         for container_status in pod.status.container_statuses or []:
             restart_count = container_status.restart_count
+            last_state = container_status.last_state
+
             if restart_count > threshold:
-                retval.append({'name': pod.metadata.name, 'namespace': pod.metadata.namespace})
+                if last_state and last_state.terminated:
+                    termination_time = last_state.terminated.finished_at
+                    # We compare if the termination time is within the last 24 hours, if yes
+                    # then we need to add it to the retval and return the list back
+                    if termination_time and termination_time >= interval_time_to_check:
+                        retval.append({'name': pod.metadata.name, 'namespace': pod.metadata.namespace})
 
     return (False, retval) if retval else (True, None)
