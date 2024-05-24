2a8c2b0379104edb01b0e74dd00db3efe9a394fe
shloka-bhalgat-unskript <shloka@unskript.com>
2024-05-24 15:28:30 +0530

    EN-5535 Update checks for time based interval check

--- Kubernetes/legos/k8s_get_pending_pods/k8s_get_pending_pods.py
+++ Kubernetes/legos/k8s_get_pending_pods/k8s_get_pending_pods.py
@@ -6,10 +6,16 @@ from typing import Optional, Tuple
 from pydantic import BaseModel, Field
 import json
 from tabulate import tabulate
+from datetime import datetime, timedelta, timezone
 
 
 class InputSchema(BaseModel):
     namespace: Optional[str] = Field('', description='k8s Namespace', title='Namespace')
+    time_interval_to_check: int = Field(
+        24,
+        description='Time interval in hours. This time window is used to check if POD was in Pending state. Default is 24 hours.',
+        title="Time Interval"
+    )
 
 
 
@@ -24,9 +30,12 @@ def k8s_get_pending_pods_printer(output):
         print(tabulate(data, headers=headers, tablefmt="grid"))
 
 
-def k8s_get_pending_pods(handle, namespace:str="") -> Tuple:
+def format_datetime(dt):
+    return dt.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+def k8s_get_pending_pods(handle, namespace: str = "", time_interval_to_check=24) -> Tuple:
     """
-    k8s_get_pending_pods checks if any pod in the Kubernetes cluster is in 'Pending' status.
+    k8s_get_pending_pods checks if any pod in the Kubernetes cluster is in 'Pending' status within the specified time interval.
 
     :type handle: object
     :param handle: Object returned from the Task validate method
@@ -34,8 +43,12 @@ def k8s_get_pending_pods(handle, namespace:str="") -> Tuple:
     :type namespace: string
     :param namespace: Namespace in which to look for the resources. If not provided, all namespaces are considered
 
+    :type time_interval_to_check: int
+    :param time_interval_to_check: (Optional) Integer, in hours, the interval within which the
+            state of the POD should be checked.
+
     :rtype: tuple
-    :return: Status,list of pending pods with their namespace
+    :return: Status, list of pending pods with their namespace and the time they became pending
     """
     if handle.client_side_validation is not True:
         print(f"K8S Connector is invalid: {handle}")
@@ -48,10 +61,14 @@ def k8s_get_pending_pods(handle, namespace:str="") -> Tuple:
     result = handle.run_native_cmd(cmd)
 
     if result.stderr:
-        raise Exception(f"Error occurred while executing command {cmd} {result.stderr}")
+        raise Exception(f"Error occurred while executing command {cmd}: {result.stderr}")
 
     pods = json.loads(result.stdout)['items']
     pending_pods = []
+
+    current_time = datetime.now(timezone.utc)
+    interval_time_to_check = current_time - timedelta(hours=time_interval_to_check)
+    interval_time_to_check = interval_time_to_check.replace(tzinfo=timezone.utc)
 
     for pod in pods:
         name = pod['metadata']['name']
@@ -59,10 +76,20 @@ def k8s_get_pending_pods(handle, namespace:str="") -> Tuple:
         pod_namespace = pod['metadata']['namespace']
 
         if status == 'Pending':
-            pending_pods.append([name, pod_namespace])
+            # Check if the pod has been in Pending state within the specified the last 24 hours
+            start_time = pod['status'].get('startTime')
+            if start_time:
+                start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
+                if start_time >= interval_time_to_check:
+                    formatted_start_time = format_datetime(start_time)
+                    formatted_interval_time_to_check = format_datetime(interval_time_to_check)
+                    pending_pods.append({
+                        "pod": name,
+                        "namespace": pod_namespace,
+                        "start_time": formatted_start_time,
+                        "interval_time_to_check": formatted_interval_time_to_check
+                    })
 
-    if len(pending_pods) != 0:
+    if pending_pods:
         return (False, pending_pods)
     return (True, None)
-
-
